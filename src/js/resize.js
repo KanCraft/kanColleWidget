f0cf1630deb920dc3f4be0d738f5c673ec80fbf9
otiai10 <otiai10@gmail.com>
2013-08-23 12:10:57 +0900

    v0.4

--- src/js/resize.js
+++ src/js/resize.js
@@ -7,23 +7,6 @@
     },1000);
 })();
 
-/* dict */function parse(query){
-    var dict = {};
-    query.replace(/^\?/,'').split(/&/).map(function(k_v){
-        var tmp = k_v.split('=');
-        dict[tmp[0]] = tmp[1];
-    });
-    return dict;
-}
-
-/* string */function dump(dict){
-    var s = '';
-    for (var k in dict){
-        s += k + "\t: " + dict[k] + "\n";
-    }
-    return s;
-}
-
 /* mode: string */function getZoom(){
     var mapWidthZoom = {
         '1200': '1.5',
