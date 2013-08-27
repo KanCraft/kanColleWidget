6a0fb1dcf6816aedab1c7ec0c9317c6037cd03a9
otiai10 <hiromu.ochiai@gree.net>
2013-08-27 10:55:13 +0900

    reflect review : fix bug of black-margin blank

--- src/js/_util.js
+++ src/js/_util.js
@@ -1,4 +1,4 @@
-function parse(query_str){
+/* dict */function parse(query_str){
     var params = {};
     query_str.replace(/^\?/,'').split('&').map(function(k_v){
         var tmp = k_v.split('=');
@@ -7,11 +7,31 @@ function parse(query_str){
     return params;
 }
 
-function dump(dict,f){
+/* dict.stringify */function dump(dict,f){
     var s = '';
     for (var k in dict){
         s += k + "\t: " + dict[k] + "\n";
     }
     if(f) return s;
     alert(s);
+}
+
+/* mode: string */function getMode(){
+    var mapWidthMode = {
+        '1200': 'l',
+        '800' : 'm',
+        '600' : 's',
+        '400' : 'xs'
+    }
+    return mapWidthMode[window.innerWidth];
+}
+
+/* zoom: string */function getZoom(){
+    var mapWidthZoom = {
+        '1200': '1.5',
+        '800' : '1',
+        '600' : '0.75',
+        '400' : '0.5'
+    }
+    return mapWidthZoom[window.innerWidth];
 }
