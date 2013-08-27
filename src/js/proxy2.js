19b799bcffe8f23dd9a3a326a26184ef244de965
otiai10 <otiai10@gmail.com>
2013-08-27 10:55:13 +0900

    reflect review : fix bug of black-margin blank

--- src/js/proxy2.js
+++ src/js/proxy2.js
@@ -5,8 +5,8 @@
         if(embed) src = embed.getAttribute('src');
         if(src){
             var conf_list = {"l": 1200,"m": 800,"s": 600,"xs": 400};
-            var mode = parse(window.location.search)['mode'];
-            var width = conf_list[mode];
+            var mode = getMode();
+            var width = parseInt(conf_list[mode]);
             var height= width*0.6;
             var w1 = window;
             var w2 = window.open(src, "_blank", "width="+width+",height="+height+",menubar=no,status=no,scrollbars=no,resizable=no,left=40,top=40");
@@ -16,4 +16,3 @@
         }
     },1000);
 })();
-
