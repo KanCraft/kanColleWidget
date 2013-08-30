197084123d6e9ec7d386e6e5aa3c79b9ab087544
otiai10 <otiai10@gmail.com>
2013-08-31 01:11:35 +0900

    remove unnecessary files

--- src/js/proxy2.js
+++ src/js/proxy2.js
@@ -1,4 +1,14 @@
 (function(){
+    //------------------------->>>
+    var body = document.getElementsByTagName('body').item().style.zoom = getZoom();
+    var wrapper = document.getElementById('flashWrap');
+    wrapper.style.margin = 0;
+    setTimeout(function(){
+        document.getElementById('sectionWrap').style.display = 'none';
+        var div = document.getElementById('spacing_top');
+        if(div) div.style.display = 'none';
+    },1000);
+    //-----------------------<<<
     var src = null;
     setTimeout(function(){
         var embed = document.getElementById('externalswf');
@@ -18,6 +28,7 @@
             window.resizeTo(width,height);
             var doc = window.document;
             doc.open();
+            //doc.write(proxy_html_string);
                 doc.write('<html><head>');
                 doc.write('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>');
                 doc.write('<script type="text/javascript">');
@@ -31,7 +42,7 @@
                 doc.write('</script>');
                 doc.write('<title>艦これウィジェット</title>');
                 doc.write('</head>');
-                doc.write('<body>');
+                doc.write('<body style="margin:0;">');
                 doc.write('<iframe id="kancolle" src='+src+' frameborder="0" scrolling="no" width="800" height="480"></iframe>');
                 doc.write('</body>');
                 doc.write('</html>');
