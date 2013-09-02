b04a4c0f0eecba9d548c59efa55a5eea7891c330
otiai10 <otiai10@gmail.com>
2013-09-02 19:38:40 +0900

    basic process for badging left time (TODO: affect config)

--- src/js/definitions/Observer.js
+++ src/js/definitions/Observer.js
@@ -7,17 +7,59 @@ function Observer(){/*** 時間管理をします ***/
     this.targets = [
         new Missions()//missions
     ];
+    this.NearestEndEvent = null; /* SoloEvent */
+    this.UpToTimeEvents   = []; /* SoloEvent[] */
 }
 
 Observer.prototype.start = function(){
-    _log('%cオブザーバだよーん（＾ω＾ ≡ ＾ω＾）%c',true);
-    for(var i=0,len=this.targets.length; i<len; i++){
-        this._start(this.targets[i]);
-    }
+    var self = this;
+    setInterval(function(){
+        self.checkAll();
+    }, 5 * 1000);
 }
 
-Observer.prototype._start = function(target){
-    setInterval(function(){
-        target.check();
-    }, 5 * 1000);
+Observer.prototype.checkAll = function(){
+    /** このforの中はなんとしてもsync堅守 */
+    for(var i= 0,len=this.targets.length; i<len; i++){
+        var task = this.targets[i];
+        var result = task.check();
+        if(this.NearestEndEvent == null){
+            this.NearestEndEvent = result.nearestEnd;
+        }
+        else if(result.nearestEnd == null){
+            // do nothing
+        }
+        else if(result.nearestEnd.getEndTime() < this.NearestEndEvent.getEndTime()){
+            this.NearestEndEvent = result.nearestEnd;
+        }else{
+            // do nothing
+        }
+        this.UpToTimeEvents = this.UpToTimeEvents.concat(result.upToTime);
+    }
+    _log('%c直近で終わるイベント%c',true);
+    _log(this.NearestEndEvent);
+    //---> で、ここでこれらを消化してクリアする
+    this.updateBadgeContext();
+    this.digestUpToTimeEvents();
+}
+
+Observer.prototype.digestUpToTimeEvents = function(){
+    _log('%c終わったイベント%c',true);
+    for(var i= 0,len=this.UpToTimeEvents.length; i<len; i++){
+        var e = this.UpToTimeEvents[i];
+        _log(e);
+        e.notify();
+    }
+    if(this.NearestEndEvent == null || this.NearestEndEvent.isUpToTime()) this.NearestEndEvent = null;
+    this.UpToTimeEvents = [];
+}
+
+Observer.prototype.updateBadgeContext = function(){
+    if(true){ // nearestをひょうじするか？
+        if(this.NearestEndEvent){
+            badgeLeftTime(this.NearestEndEvent.getEndTime());
+        }
+    }else{ // upToTImeの数を表示するか？
+        console.log('ここでupToTimeの数をだな');
+    }
 }
