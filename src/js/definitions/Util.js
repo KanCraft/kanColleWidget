0699667b8a7828d67f27893272fa80cec6b12c35
otiai10 <otiai10@gmail.com>
2013-09-02 09:57:54 +0900

    prepare for 'more config'

--- src/js/definitions/Util.js
+++ src/js/definitions/Util.js
@@ -45,13 +45,16 @@
     return parseInt(window.navigator.appVersion.match(/Chrome\/(\d+)\./)[1], 10);
 }
 
-/* void */function focusKCWidgetWindow(){
+/* void */function focusKCWidgetWindow(cb){
+    if(!cb) cb = function(){};
     chrome.windows.getAll({populate:true},function(windows){
         for(var i in windows){
             var w = windows[i];
             if(!w.tabs || w.tabs.length < 1) return;
             if(w.tabs[0].url.match(/^http:\/\/osapi.dmm.com\/gadgets\/ifr/)){
-                chrome.windows.update(w.id,{focused:true});
+                chrome.windows.update(w.id,{focused:true}, function(){
+                    cb();
+                });
             }
         }
     });
