b532e8ef74c29109bd66549ba33be5f8eeef5d0a
otiai10 <otiai10@gmail.com>
2013-09-04 21:03:16 +0900

    bind action for api_req_kousyou/createship

--- src/js/definitions/Dispatcher.js
+++ src/js/definitions/Dispatcher.js
@@ -39,6 +39,9 @@ function Dispatcher(data){/** パースの結果をラップします **/
         case 'api_req_kaisou/powerup':
             this.action.forKaisouPowerup(this.params);
             break;
+        case 'api_req_kousyou/createship':
+            this.action.forKousyouCreateship(this.params);
+            break;
         default:
             //_log('%c[ACTION]%c Do Nothing for this request',true);
     }
