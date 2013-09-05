6d4ac6d6d28272fd4df48d6b8473857b687f991e
otiai10 <otiai10@gmail.com>
2013-09-05 17:31:13 +0900

    see rich config to open manual popup

--- src/js/definitions/models/MyStorage.js
+++ src/js/definitions/models/MyStorage.js
@@ -13,3 +13,21 @@ function MyStorage(){/** localStorageにアクセスするクラス **/}
 /* void (でいいのか？) */MyStorage.prototype.set = function(key,value){
     localStorage.setItem(key,JSON.stringify(value));
 }
+
+/* static */var Config = {/** localStorage.configにアクセスするstaticなクラス **/
+
+    /* private */storage : new MyStorage(),
+
+    /* public: dict */get : function(key){
+        var config = this.storage.get('config');
+        if(typeof config[key] == 'undefined') return;
+        return config[key];
+    },
+    /* public: bool */set : function(key,value){
+        var config = this.storage.get('config');
+        if(typeof config[key] == 'undefined') return false;// throw new Error('cannot set on undefined');
+        config[key] = value;
+        this.storage.set('config', config);
+        return true;
+    }
+}
