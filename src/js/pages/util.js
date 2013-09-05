19a4c9739a4e6a50ab07aa56c06f05b243883686
otiai10 <otiai10@gmail.com>
2013-09-05 19:35:54 +0900

    rename _util to util

--- src/js/pages/util.js
+++ src/js/pages/util.js
@@ -12,3 +12,25 @@
     if(f) return s;
     alert(s);
 }
+
+/* zoom: string */function getZoom(){
+    return Constants.widget.width[window.innerWidth].zoom;
+}
+
+/* dict.stringify */function dump(dict,f){
+    var s = '';
+    for (var k in dict){
+        s += k + "\t: " + dict[k] + "\n";
+    }
+    if(f) return s;
+    alert(s);
+}
+
+/* dict */function parse(query_str){
+    var params = {};
+    query_str.replace(/^\?/,'').split('&').map(function(k_v){
+        var tmp = k_v.split('=');
+        params[tmp[0]] = tmp[1];
+    });
+    return params;
+}
