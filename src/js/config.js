83638989538538ad4ae76c07f3a7ef1f2c330964
about518 <iemon29@gmail.com>
2013-09-05 15:33:41 +0900

    通知時のサウンド再生

--- src/js/config.js
+++ src/js/config.js
@@ -1,20 +1,68 @@
-(function(){
-    var anchors = document.getElementsByTagName('a');
-    for(var i=0, len=anchors.length; i<len;i++){
-        anchors[i].addEventListener('click',function(e){
-            chrome.tabs.create({url:this.getAttribute('href')});
-        });
-    }
-    var checkbox = document.getElementById('doAlert');
-    updateChecked();
-    checkbox.addEventListener('change',function(e){
-        localStorage.setItem('config_showAlert', this.checked);
-        updateChecked();
-    });
+var myStorage = new MyStorage();
+(function() {
+	initConfig();
+	var config = myStorage.get('config');
+	affectConfigInView(config);
+	bindCloseAction();
+	bindConfigChangedAction();
 })();
 
-function updateChecked(){
-    var checkbox = document.getElementById('doAlert');
-    if(localStorage.getItem('config_showAlert') == 'true') checkbox.setAttribute('checked');
-    else checkbox.removeAttribute('checked');
+function initConfig() {
+	var initial_config = {
+		'alert-campaign' : true,
+		'play-sound-at-alert' : false,
+		'file-sound-at-alert' : "",
+		'volume-sound-at-alert' : "100",
+		'disp-image-at-alert' : false,
+		'file-image-at-alert' : "",
+		'badge-left-time' : false,
+		'record-achievements' : false
+	};
+	// var config = initial_config;
+	var config = myStorage.get('config') || initial_config;
+	myStorage.set('config', config);
+}
+function affectConfigInView(config) {
+	for ( var key in config) {
+		var input = document.getElementById(key);
+		if (input) {
+			if (input.files) {
+				// ファイルなら
+				input.files[0] = config[key];
+			} else if (input.checked != undefined
+					&& typeof config[key] == 'boolean') {
+				// チェックボックスなら
+				input.checked = config[key];
+			} else if (input.value != undefined) {
+				// チェックボックス以外
+				input.value = config[key];
+			}
+		}
+	}
+}
+function bindCloseAction() {
+	document.getElementById('close-config').addEventListener('click',
+			function() {
+				window.close();
+			});
+}
+function bindConfigChangedAction() {
+	var inputs = document.getElementsByTagName('input');
+	for ( var i = 0, len = inputs.length; i < len; i++) {
+		inputs[i].addEventListener('change', function() {
+			var config = myStorage.get('config');
+			if (this.files) {
+				// ファイルなら
+				config[this.id] = this.files[0];
+			} else if (this.checked != undefined
+					&& typeof config[this.id] == 'boolean') {
+				// チェックボックスなら
+				config[this.id] = this.checked;
+			} else if (this.value != undefined) {
+				// チェックボックス以外
+				config[this.id] = this.value;
+			}
+			myStorage.set('config', config);
+		});
+	}
 }
--- src/js/select.js
+++ src/js/select.js
@@ -93,7 +93,7 @@ var map_key_lang = {
             var w = conf_list[mode];
             var options = "width={w},height={h},menubar=no,status=no,scrollbars=no,resizable=no,left=40,top=40".replace('{w}', w).replace('{h}', String(w * aspect));
             var kanColleUrl = 'https://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/?mode='+mode;
-            window.open(kanColleUrl,"_blank_new", options)
+            window.open(kanColleUrl,"_blank", options);
             window.close();
         });
     });
