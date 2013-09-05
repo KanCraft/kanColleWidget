43ae9c720a71733fbc97f90a226f1a48c1a41e1d
otiai10 <otiai10@gmail.com>
2013-09-05 20:20:36 +0900

    create base class of storaged events

--- src/js/definitions/models/events/createships/Createships.js
+++ src/js/definitions/models/events/createships/Createships.js
@@ -1,59 +1,16 @@
-/**
- * dependency: MyStorage
- */
-
 /***** class definitions *****/
-function Createships(){/** localStorageにあるcreateshipsにアクセスするクラス **/}
+function Createships(){/** localStorageにあるcreateshipsにアクセスするクラス **/
+    this.primaryIdName = 'api_kdock_id';
+    this.storageName   = 'createships';
+    this.soloModel     = SoloCreateship;
+    this.initialValue  = [
+        {api_kdock_id: 1, finish: null},
+        {api_kdock_id: 2, finish: null},
+        {api_kdock_id: 3, finish: null},
+        {api_kdock_id: 4, finish: null}
+    ];
+}
 
 // extend
-Createships.prototype = Object.create(MyStorage.prototype);
+Createships.prototype = Object.create(EventsBase.prototype);
 Createships.prototype.constructor = Createships;
-
-/* saved: Dict */Createships.prototype.add = function(api_kdock_id, finishTime){
-    if(this.get('createships') == undefined) {
-        var initialValue = [{api_kdock_id: 1, finish: null}, {api_kdock_id: 2, finish: null}, {api_kdock_id: 3, finish: null}, {api_kdock_id: 4, finish: null}];
-        this.set('createships', initialValue);
-    }
-    var createships = this.get('createships');
-    for(var i = 0;i < createships.length;i++) {
-        if(createships[i].api_kdock_id == api_kdock_id)
-            createships[i].finish = finishTime;
-    }
-    this.set('createships', createships);
-}
-
-/* success: Boolean */Createships.prototype.clear = function(api_kdock_id){
-    if(this.get('createships') == undefined) return;
-    var createships = this.get('createships');
-    for(var i = 0;i < createships.length;i++) {
-        if(createships[i].api_kdock_id == api_kdock_id)
-            createships[i].finish = null;
-    }
-    this.set('createships', createships);
-}
-
-/* dict{nearestEndCreateship, upToTimeCreateship} */Createships.prototype.check = function(){
-    var result = {
-        nearestEnd : null,
-        upToTime   : []
-    };
-    if(this.get('createships') == undefined) return result;
-    var createships = this.get('createships');
-    for(var i= 0,len=createships.length; i<len;i++){
-        var cjson = createships[i];
-        if(cjson.finish == null) continue;
-        var c = new SoloCreateship(cjson);
-        if(c.isUpToTime()){
-            this.clear(c.api_kdock_id);
-            result.upToTime.push(c);
-        }
-        if(result.nearestEnd == null){
-            result.nearestEnd = c;
-        }else if(c.getEndTime() < result.nearestEnd.getEndTime()){
-            result.nearestEnd = c;
-        }else{
-            // do nothing
-        }
-    }
-    return result;
-}
