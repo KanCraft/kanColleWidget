87a90ea81b861abbd925a00395549e3b96e1ce2f
otiai10 <otiai10@gmail.com>
2013-09-07 08:04:06 +0900

    Feedback UI for Reminder Commit

--- src/js/pages/set_createship.js
+++ src/js/pages/set_createship.js
@@ -9,8 +9,8 @@
         var createships = new Createships();
         createships.add(params.api_kdock_id, params.finish);
         updateInputTracking('createship');
-        _presentation('建造作業完了通知を登録しときました',false,function(){
-            window.close();
+        _presentation('建造作業完了通知を登録しときました',false,{
+            callback : function(){ window.close(); }
         });
     });
     delegateCancelAction();
