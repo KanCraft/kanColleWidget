60622ca90ad526d046e6bb60a92b1bcc665d9089
otiai10 <otiai10@gmail.com>
2013-09-06 15:00:33 +0900

    improve : Presentation when reminder is committed

--- src/js/pages/set_createship.js
+++ src/js/pages/set_createship.js
@@ -3,12 +3,15 @@
     applyView(params);
     affectInputTracking('createship');
     delegateCarretActions();
-    delegateCommitAction('createship', function(finish_time){
+    delegateCommitAction(function(finish_time){
         params.finish = finish_time;
         // Createshipsモデルを呼び出してaddする
         var createships = new Createships();
         createships.add(params.api_kdock_id, params.finish);
-        window.close();
+        updateInputTracking('createship');
+        _presentation('建造作業完了通知を登録しときました',false,function(){
+            window.close();
+        });
     });
     delegateCancelAction();
 })();
