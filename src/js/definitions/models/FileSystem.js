75e8049731fdb2ad03f92160be2ed4ef9b690243
otiai10 <otiai10@gmail.com>
2013-09-08 14:36:46 +0900

    Capsulize Fs methods

--- src/js/definitions/models/FileSystem.js
+++ src/js/definitions/models/FileSystem.js
@@ -1,80 +1,179 @@
-var SIZE_LIMIT = 1024 * 1024 * 5; // 5MB
 var requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
 
-function onFileError(error){
-	console.log("File Error: "+ error.code);
-	for ( var key in error){
-		console.log(key + "=" + error[key]);
-	}
-}
+var Fs = {
 
-function readFile(fileName, callbackFunc){
-	console.log("readFile: " + fileName);
-	requestFileSystem(PERSISTENT, SIZE_LIMIT, function(fileSystem){
-		fileSystem.root.getFile(fileName, {}, function(fileEntry){
-			fileEntry.file(function(file){
-				var reader = new FileReader();
-				reader.onloadend = function(event){
-					console.log("read completed. "+ fileEntry.toURL());
-					if( callbackFunc ){
-						callbackFunc(fileEntry);
-					}
-				};
-				reader.onerror = function(error){
-					alert("read failed : " + error);
-				};
-				reader.readAsBinaryString(file);
-			}, onFileError);
-		}, onFileError);
-	}, onFileError);
-}
+    // レスポンス＼(^o^)／オブジェクト
+    Response : {
+        /* string */  purpose : '',
+        /* int */      status : 0,
+        /* string */  message : '',
+        /* Error */     error : null,
+        /* FileEntry */ entry : null, //保存されたエントリ
+        /* File */     origin : null //選択されたオリジナルなファイルエントリ
+    },
+    // レスポンスオブジェクト作るメソッド
+    /* private */responseFactory : /* this.Response */function(status, message, entry, error){
+        this.Response.status = status;
+        this.Response.message = message;
+        this.Response.error = error || null;
+        this.Response.entry = entry || null;
+        this.Response.origin = this.origin;
+        this.Response.purpose = this.purpose;
+        return this.Response;
+    },
 
-function writeFile(file, callbackFunc) {
-	console.log("writeFile " + file.name);
-	requestFileSystem(PERSISTENT, SIZE_LIMIT, function(fileSystem){
-		console.log("writeFile: initFileSystem");
-		fileSystem.root.getFile(file.name, { create: true, exclusive: false }, function(fileEntry){
-			fileEntry.createWriter(function(fileWriter){
-				fileWriter.onwriteend = function(event){
-					console.log("write completed. " + fileEntry.toURL());
-					if( callbackFunc ){
-						callbackFunc(fileEntry);
-					}
-				};
-				fileWriter.onerror = function(error){
-					alert("write failed : " + error);
-				};
-				fileWriter.write(file);
-			}, onFileError);
-		}, onFileError);
-	}, onFileError);
-	return true;
-}
+    // 受付可能なファイルタイプ
+    accept : null,
+    // 保存目的
+    purpose : null,
+    // 最終保存パス
+    destination : null,
 
-function deleteFile(file){
-	console.log("deleteFile " + file.name);
-	requestFileSystem(PERSISTENT, SIZE_LIMIT, function(fileSystem){
-		console.log("deleteFile: initFileSystem");
-		fileSystem.root.getFile(file.name, { create: false }, function(fileEntry){
-			fileEntry.remove(function(){
-				console.log("delete completed. " + fileEntry.toURL());
-			}, onFileError);
-		}, onFileError);
-	}, onFileError);
-}
+    // サイズ制限
+    SIZE_LIMIT : 1024 * 1024 * 5, // 5MB
 
-function writeLocalFile(file, accept, callbackFunc){
-	console.log("writeLocalFile: " + file.name);
-	for( var key in file ){
-		console.log(key + "=" + file[key]);
-	}
-	if( !file.type.match(accept) ){
-		alert("ファイル種別が違います");
-		return false;
-	}
-	if( file.size > SIZE_LIMIT ){
-		alert("ファイルサイズが大きすぎます。"+SIZE_LIMIT/(1024*1024)+"MB未満のファイルにしてください。");
-		return false;
-	}
-	return writeFile(file, callbackFunc);
+    // エラーハンドリング
+    /* private */onError : /* this.Response */function(error){
+        var message = '';
+        for ( var key in error){
+            message += key + "=" + error[key] + ',';
+        }
+        return this.responseFactory(0, message, null, error);
+    },
+
+    // バリデーション
+    /* private */validate : /* bool */function(file){
+        if(typeof file == 'undefined'){
+            this.Response.status = 0;
+            this.Response.message = "キャンセルされましたー";
+            return false;
+        }
+        if( !file.type.match(this.accept) ){
+            this.Response.status = 0;
+            this.Response.message = "ファイル種別が違います";
+            return false;
+        }
+        if( file.size > this.SIZE_LIMIT ){
+            this.Response.status = 0;
+            this.Response.message = "ファイルサイズが大きすぎます。"+ this.SIZE_LIMIT/(1024*1024) +"MB未満のファイルにしてください。";
+            return false;
+        }
+        return true;
+    },
+
+    // 保存ファイルパスを決定
+    /* private */defineDestination : function(){
+        var ext = this.origin.type.replace(/^[a-zA-Z]+\//,'');
+        return this.purpose + '.' + ext;
+    },
+
+    // 削除 /* public */
+    // delete : function(){},
+    // 読み込み /* public */
+    // read : function(){},
+
+    // 書き込み /* public */
+    write : /* f(this.Response) */function(purpose, file, accept, callbackFunc){
+
+        this.purpose = purpose;
+        this.origin = file;
+        this.accept = accept;
+        this.callback = callbackFunc;
+
+        if(!this.validate(file)) return this.callback(this.Response);
+
+        //for( var key in file ){
+        //    console.log(key + "=" + file[key]);
+        //}
+
+        this.destination = this.defineDestination();
+
+        var self = this;
+        requestFileSystem(
+            window.PERSISTENT,
+            self.SIZE_LIMIT,
+            function(fileSystem){
+                // オリジナルな名前で保存するとfilesystem:chrome-extension://***/persistent/以下がヤバい
+                fileSystem.root.getFile(
+                    self.destination,
+                    { create: true, exclusive: false },
+                    function(fileEntry){
+                        fileEntry.createWriter(function(fileWriter){
+
+                            // add listeners
+                            fileWriter.onwriteend = function(event){
+                                self.responseFactory(1,'Write completed',fileEntry,null);
+                                self.callback(self.Response);
+                            };
+                            fileWriter.onerror = self.onError;
+
+                            // execute
+                            fileWriter.write(file);
+
+                        }, self.onError);
+                    },
+                    self.onError
+                );
+            },
+            self.onError
+        );
+    },
+
+    // TODO: 上記writeとDRYにする
+    // 更新 /* public */
+    update : /* f(this.Response) */function(purpose, file, accept, callbackFunc){
+
+        this.purpose = purpose;
+        this.origin = file;
+        this.accept = accept;
+        this.callback = callbackFunc;
+
+        if(!this.validate(file)) return this.callback(this.Response);
+
+        //for( var key in file ){
+        //    console.log(key + "=" + file[key]);
+        //}
+
+        this.destination = this.defineDestination();
+
+        var self = this;
+        self.delete(
+            this.destination,
+            function(){
+                self.write(purpose,file,accept,callbackFunc);
+            },
+            function(){
+                //console.log('多分初回はremoveでエラー');
+                // 雑だなー
+                self.write(purpose,file,accept,callbackFunc);
+            }
+        );
+    },
+
+    // 削除 /* private */
+    delete : /* f() */function(f_path, success_callback, error_callback){
+        var self = this;
+        requestFileSystem(
+            window.PERSISTENT,
+            self.SIZE_LIMIT,
+            function(fileSystem){
+                fileSystem.root.getFile(
+                    f_path,
+                    { create: true, exclusive: false },
+                    function(fileEntry){
+                        fileEntry.remove(
+                            function(){
+                                success_callback();
+                            },
+                            function(){
+                                error_callback()
+                            }
+                        );
+                    },
+                    self.onError
+                );
+            },
+            self.onError
+        );
+    }
 }
