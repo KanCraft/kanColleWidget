25ad513760a1904fb74db3983830175d1c3c5882
otiai10 <otiai10@gmail.com>
2013-09-26 15:54:35 +0900

    notify offset

--- src/js/definitions/models/events/SoloEventBase.js
+++ src/js/definitions/models/events/SoloEventBase.js
@@ -6,7 +6,7 @@ function SoloEventBase(){}
     // migration : mission.finish (date object -> epoch msec)
     if(typeof this.finish != 'number') this.finish = (new Date(this.finish)).getTime();
 
-    var finish = this.finish;
+    var finish = this.finish - Constants.time.notifyOffset;
 
     return (finish < now);
 }
