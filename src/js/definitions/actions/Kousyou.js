cad9bbdcdcedfff3603daf6278fc40674bb3d681
otiai10 <otiai10@gmail.com>
2013-09-27 10:28:59 +0900

    judge capture timing by request sequence

--- src/js/definitions/actions/Kousyou.js
+++ src/js/definitions/actions/Kousyou.js
@@ -12,28 +12,54 @@ KousyouAction.prototype.forCreateship = function(params){
 
     this.achievements.update().incrementCreateshipCount();
 
+    Stash.params = params;
+
     // 高速建造を使用する
     if(params.api_highspeed == 1) return;
 
     // 何もしないひと
-    if(!Config.get('dynamic-reminder-type') || Config.get('dynamic-reminder-type') == 0) return;
-
+    if(Config.get('dynamic-reminder-type') == 0) return;
     // 常にマニュアル登録のひと
     if(Config.get('dynamic-reminder-type') == 1) return Util.enterTimeManually(params,'src/html/set_createship.html');
 
     // 他、自動取得しようとするひと
     var loadingWindow = Util.openLoaderWindow();
+
+    Stash.loadingWindow = loadingWindow;
     Util.adjustSizeOfWindowsOS(loadingWindow);
+
+}
+KousyouAction.prototype.forGetship = function(params){
+    var createships = new Createships();
+    createships.clear(params.api_kdock_id);
+}
+KousyouAction.prototype.forCreateitem = function(params){
+    this.achievements.update().incrementCreateitemCount();
+}
+
+// Completed
+KousyouAction.prototype.forCreateshipCompleted = function(){
+
+    // 何もしないひと
+    if(Config.get('dynamic-reminder-type') == 0) return;
+    // 常にマニュアル登録のひと
+    if(Config.get('dynamic-reminder-type') == 1) return;
+
     var callback = function(res){
-        loadingWindow.close();
+
+        // 遅らせてローディング画面を閉じる
+        setTimeout(function(){
+            Stash.loadingWindow.close();
+        },600);
+
         var finishTimeMsec = Util.timeStr2finishEpochMsec(res.result);
         console.log(res);
         if(!finishTimeMsec){
             if(!window.confirm("建造終了時間の取得に失敗しました" + Constants.ocr.failureCause + "\n\n手動登録しますか？")) return;
-            return Util.enterTimeManually(params,'src/html/set_createship.html');
+            return Util.enterTimeManually(Stash.params,'src/html/set_createship.html');
         }
         var createships = new Createships();
-        createships.add(params.api_kdock_id[0], finishTimeMsec);
+        createships.add(Stash.params.api_kdock_id[0], finishTimeMsec);
 
         if(!Config.get('notification-on-reminder-set')) return;
 
@@ -46,16 +72,9 @@ KousyouAction.prototype.forCreateship = function(params){
             Util.extractFinishTimeFromCapture(
                 widgetWindow.id,
                 'createship',
-                params.api_kdock_id[0],
+                Stash.params.api_kdock_id[0],
                 callback
             );
         });
-    }, Constants.ocr.delay);
-}
-KousyouAction.prototype.forGetship = function(params){
-    var createships = new Createships();
-    createships.clear(params.api_kdock_id);
-}
-KousyouAction.prototype.forCreateitem = function(params){
-    this.achievements.update().incrementCreateitemCount();
+    }, 400); //単に描画時間を待つ
 }
