5e71bf552f2d70d8f776d88c9fe4ca033fea90b4
otiai10 <otiai10@gmail.com>
2013-10-12 00:24:03 +0900

    refactor

--- src/js/definitions/models/achievements/Achievements.js
+++ src/js/definitions/models/achievements/Achievements.js
@@ -14,7 +14,7 @@ Achievements.prototype.constructor = Achievements;
     if(typeof target == 'undefined') target = 'all';
     var initial_achievements = {
         'daily' : {
-            'lastUpdated' : Util.getNearestDailyAchievementResetTime(),
+            'lastUpdated' : this.getNearestDailyAchievementResetTime(),
             'contents' : {
                 mission_count   : 0,
                 practice_count  : 0,
@@ -26,7 +26,7 @@ Achievements.prototype.constructor = Achievements;
             }
         },
         'weekly' : {
-            'lastUpdated' : Util.getNearestWeeklyAchievementResetTime(),
+            'lastUpdated' : this.getNearestWeeklyAchievementResetTime(),
             'contents' : {
                 mission_count   : 0,
                 practice_count  : 0,
@@ -38,15 +38,18 @@ Achievements.prototype.constructor = Achievements;
             }
         }
     };
+
     var achievements_json = this.get('achievements') || initial_achievements;
-    if(achievements_json.daily.lastUpdated < Util.getNearestDailyAchievementResetTime()){
-        achievements_json.daily.lastUpdated = (new Date()).getTime();
+
+    if(achievements_json.daily.lastUpdated < this.getNearestDailyAchievementResetTime()){
+        achievements_json.daily.lastUpdated = Date.now();
         achievements_json.daily.contents = initial_achievements.daily.contents;
     }
-    if(achievements_json.weekly.lastUpdated < Util.getNearestWeeklyAchievementResetTime()){
-        achievements_json.weekly.lastUpdated = (new Date()).getTime();
+    if(achievements_json.weekly.lastUpdated < this.getNearestWeeklyAchievementResetTime()){
+        achievements_json.weekly.lastUpdated = Date.now();
         achievements_json.weekly.contents = initial_achievements.weekly.contents;
     }
+
     if(force == true){
         if(target == 'all'){
             achievements_json = initial_achievements;
@@ -102,3 +105,17 @@ Achievements.prototype.constructor = Achievements;
     return this.get('achievements');
 }
 
+/* epoch: msec */Achievements.prototype.getNearestDailyAchievementResetTime = function(){
+    var now = new Date();
+    var diffHours = (now.getHours() + 19) % 24;
+    var _1hourMsec = 1*60*60*1000;
+    var last5am = new Date(now - diffHours * _1hourMsec);
+    return (new Date(1900 + last5am.getYear(), last5am.getMonth(), last5am.getDate(), 5, 0)).getTime();
+}
+/* epoch: msec */Achievements.prototype.getNearestWeeklyAchievementResetTime = function(){
+    var now = new Date();
+    var diffDays = (now.getDay() + 6) % 7;
+    var _1dayMsec = 1*24*60*60*1000;
+    var lastMonday = new Date(now - diffDays * _1dayMsec);
+    return (new Date(1900 + lastMonday.getYear(), lastMonday.getMonth(), lastMonday.getDate(), 5, 0)).getTime();
+}
