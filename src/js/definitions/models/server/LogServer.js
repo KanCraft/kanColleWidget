1f38dbf324ddf729f18498fcef2894555afefb1a
otiai10 <otiai10@gmail.com>
2013-10-21 16:23:24 +0900

    add debug comment

--- src/js/definitions/models/server/LogServer.js
+++ src/js/definitions/models/server/LogServer.js
@@ -5,6 +5,8 @@
  * @constructor
  */
 function Log(){
+    // デバグにはこのサーバURLをつかうこと！！！ServerBaseでurl.matchしてる！！
+    // this.baseurl = 'http://log-kcwidget.oti10.com:7778';
     this.baseurl = 'http://log-kcwidget.oti10.com';
 };
 Log.prototype = new ServerBase(Constants);
