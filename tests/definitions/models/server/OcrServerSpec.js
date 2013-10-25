5a373c4dac42700f828909f096c5eadc5da2586c
otiai10 <otiai10@gmail.com>
2013-10-25 17:03:05 +0900

    Add OcrServer test

--- tests/definitions/models/server/OcrServerSpec.js
+++ tests/definitions/models/server/OcrServerSpec.js
@@ -1,16 +1,30 @@
 describe("OcrServer", function() {
-
+  var data, flag;
   var ocrServer;
 
   beforeEach(function() {
+    data = {};
+    flag = false;
     ocrServer = new Ocr();
   });
  
-  it("should be true.", function() {
-    for (var i in ocrServer) {
-        console.log(i, ocrServer[i]);
-    }
-    console.log(typeof ocrServer.send);
-    expect(true).toBe(true);
+  it("can OCR base64-encoded imageURI.", function() {
+    var imgURI = Fixture.base64img.sample000;
+
+    runs(function(){
+      ocrServer.send(imgURI, function(resdata){
+        data = resdata;
+        flag = true;
+      });
+    });
+    waitsFor(function(){
+      return flag;
+    }, "in 1 second", 1000);
+  });
+
+  runs(function() {
+    expect(data.message).toBe('DONE');
+    expect(data.status).toBe(200);
+    expect(data.result).toBe('00:59:43');
   });
 });
