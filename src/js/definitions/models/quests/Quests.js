6825d8cd866ae5fe6411c4bffeb9d91bbf43114a
otiai10 <otiai10@gmail.com>
2013-11-16 03:50:32 +0900

    Consider quest appearance conditions

--- src/js/definitions/models/quests/Quests.js
+++ src/js/definitions/models/quests/Quests.js
@@ -10,6 +10,32 @@ Quests.state = {
   NOW  : 1,  
   DONE : 2 
 };
+// 常にtrueを返す
+Quests._true = function(){
+    return true;
+};
+// 日付の一の位が3,7,0の時にtrueを返す
+Quests._370 = function(){
+    var today = Util.getTodayOfKanColle();
+    switch(today.getDate() % 10){
+        case 0:
+        case 3:
+        case 7:
+            return true;
+    }
+    return false;
+};
+// 日付の一の位が2,8の時にtrueを返す
+Quests._28 = function(){
+    var today = Util.getTodayOfKanColle();
+    switch(today.getDate() % 10){
+        case 2:
+        case 8:
+            return true;
+    }
+    return false;
+};
+
 
 Quests.prototype.embark = function(questId){
   return this._updateStateById(questId, Quests.state.NOW);
@@ -47,12 +73,22 @@ Quests.prototype.haveUpdate = function(criteriaTime){
 };
 Quests.prototype.getAll = function(){
     this._resetDailyIfOutdated();
-    return this.get("quests") || this.init();
+    var withMeta = this.get("quests") || this.init();
+    var all = withMeta.map;
+    var actives = {};
+    for (var i in all) {
+        var q = all[i];
+        if (Quests[q.type]()) {
+            actives[q.id] = q;
+        }
+    }
+    withMeta.map = actives;
+    return withMeta;
 };
 Quests.prototype.availables = function(){
     this._resetDailyIfOutdated();
     var all = this.get("quests").map || this.init().map;
-    var availables = [];
+    var availables = {};
     for (var i in all) {
         var q = all[i];
         // 依存するものがなければavailablesである
@@ -83,29 +119,29 @@ Quests.prototype.initialValue = {
         // TODO: デイリーかウィークリーか判別するアレ =>  (　ﾟ∀ﾟ)o彡° YAGNI！YAGNI！
         // デイリー
         // 出撃
-        201 : { title : "敵艦隊を撃破せよ！",             id : 201, required : null, state : Quests.state.YET},
-        216 : { title : "敵艦隊主力を撃滅せよ！",         id : 216, required : 201,  state : Quests.state.YET},
-        210 : { title : "敵艦隊を10回邀撃せよ！",         id : 210, required : 216,  state : Quests.state.YET},
-        211 : { title : "敵空母を3隻撃沈せよ",            id : 211, required : 201,  state : Quests.state.YET},
-        218 : { title : "敵補給艦を3隻撃沈せよ！",        id : 218, required : 216,  state : Quests.state.YET},
-        226 : { title : "南西諸島海域の制海権を握れ！",   id : 226, required : 218,  state : Quests.state.YET},
-        230 : { title : "敵潜水艦を制圧せよ！",           id : 230, required : 226,  state : Quests.state.YET},
+        201 : { title : "敵艦隊を撃破せよ！",             id : 201, required : null, state : Quests.state.YET, type : "_true" },
+        216 : { title : "敵艦隊主力を撃滅せよ！",         id : 216, required : 201,  state : Quests.state.YET, type : "_true" },
+        210 : { title : "敵艦隊を10回邀撃せよ！",         id : 210, required : 216,  state : Quests.state.YET, type : "_true" },
+        211 : { title : "敵空母を3隻撃沈せよ",            id : 211, required : 201,  state : Quests.state.YET, type : "_true" },
+        218 : { title : "敵補給艦を3隻撃沈せよ！",        id : 218, required : 216,  state : Quests.state.YET, type : "_370" },
+        226 : { title : "南西諸島海域の制海権を握れ！",   id : 226, required : 218,  state : Quests.state.YET, type : "_true" },
+        230 : { title : "敵潜水艦を制圧せよ！",           id : 230, required : 226,  state : Quests.state.YET, type : "_true" },
         // 演習
-        303 : { title : "「演習」で練度向上！",           id : 303, required : null, state : Quests.state.YET},
-        304 : { title : "「演習」で他提督を圧倒せよ！",   id : 304, required : 303,  state : Quests.state.YET},
+        303 : { title : "「演習」で練度向上！",           id : 303, required : null, state : Quests.state.YET, type : "_true" },
+        304 : { title : "「演習」で他提督を圧倒せよ！",   id : 304, required : 303,  state : Quests.state.YET, type : "_true" },
         // 遠征
-        402 : { title : "「遠征」を3回成功させよう！",    id : 402, required : null, state : Quests.state.YET},
-        403 : { title : "「遠征」を10回成功させよう！",   id : 403, required : 402,  state : Quests.state.YET},
+        402 : { title : "「遠征」を3回成功させよう！",    id : 402, required : null, state : Quests.state.YET, type : "_true" },
+        403 : { title : "「遠征」を10回成功させよう！",   id : 403, required : 402,  state : Quests.state.YET, type : "_true" },
         // 補給・入渠
-        503 : { title : "艦隊大整備！",                   id : 503, required : null, state : Quests.state.YET},
-        504 : { title : "艦隊酒保祭り！",                 id : 504, required : 503,  state : Quests.state.YET},
+        503 : { title : "艦隊大整備！",                   id : 503, required : null, state : Quests.state.YET, type : "_true" },
+        504 : { title : "艦隊酒保祭り！",                 id : 504, required : 503,  state : Quests.state.YET, type : "_true" },
         // 工廠
-        605 : { title : "新装備「開発」指令",             id : 605, required : null, state : Quests.state.YET},
-        606 : { title : "新造艦「建造」指令",             id : 606, required : 605,  state : Quests.state.YET},
-        607 : { title : "装備「開発」集中強化！",         id : 607, required : 606,  state : Quests.state.YET},
-        608 : { title : "艦娘「建造」艦隊強化！",         id : 608, required : 607,  state : Quests.state.YET},
-        609 : { title : "軍縮条約対応！",                 id : 609, required : 608,  state : Quests.state.YET},
+        605 : { title : "新装備「開発」指令",             id : 605, required : null, state : Quests.state.YET, type : "_true" },
+        606 : { title : "新造艦「建造」指令",             id : 606, required : 605,  state : Quests.state.YET, type : "_true" },
+        607 : { title : "装備「開発」集中強化！",         id : 607, required : 606,  state : Quests.state.YET, type : "_true" },
+        608 : { title : "艦娘「建造」艦隊強化！",         id : 608, required : 607,  state : Quests.state.YET, type : "_true" },
+        609 : { title : "軍縮条約対応！",                 id : 609, required : 608,  state : Quests.state.YET, type : "_true" },
         // 改装
-        702 : { title : "艦の「近代化改修」を実施せよ！", id : 702, required : null, state : Quests.state.YET}
+        702 : { title : "艦の「近代化改修」を実施せよ！", id : 702, required : null, state : Quests.state.YET, type : "_true" }
     }
 };
