97497813024f87cee60718fbcbf75759c76c1613
otiai10 <otiai10@gmail.com>
2013-11-25 20:43:56 +0900

    Observe Sorties Events

--- src/js/definitions/models/events/sortie/Sortie.js
+++ src/js/definitions/models/events/sortie/Sortie.js
@@ -1,10 +1,14 @@
 /***** class definitions *****/
-function SoloSortie(sortieJson){
-    this.primaryId = sortieJson.api_kdock_id;
-    this.finish    = sortieJson.finish;
-    this.prefix    = Constants.notification.sortie.end_prefix;
-    this.suffix    = Config.get('notification-sortie-end-suffix') || Constants.notification.sortie.end_suffix;
-    this.kind      = 'sortie-finish';
-}
-SoloSortie.prototype = Object.create(SoloEventBase.prototype);
-SoloSortie.prototype.constructor = SoloSortie;
+var KanColleWidget = KanColleWidget || {};
+
+(function(){
+    var SoloSortie = KanColleWidget.SoloSortie = function(sortieJson){
+        this.primaryId = sortieJson.api_deck_id;
+        this.finish    = sortieJson.finish;
+        this.prefix    = Constants.notification.sortie.end_prefix;
+        this.suffix    = Config.get('notification-sortie-end-suffix') || Constants.notification.sortie.end_suffix;
+        this.kind      = 'sortie-finish';
+    }
+    SoloSortie.prototype = Object.create(SoloEventBase.prototype);
+    SoloSortie.prototype.constructor = SoloSortie;
+})();
