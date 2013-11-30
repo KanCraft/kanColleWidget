f687baefaa7a10d8e20a46c52be571a3c98ca349
otiai10 <otiai10@gmail.com>
2013-11-30 14:37:46 +0900

    Fix bug : mute PreCheck alert in QuestList

--- src/js/definitions/Dispatcher.js
+++ src/js/definitions/Dispatcher.js
@@ -67,6 +67,10 @@ function Dispatcher(data){/** パースの結果をラップします **/
         case 'api_req_quest/stop':
             this.action.forQuestStop(this.params);
             break;
+        case 'api_get_member/questlist':
+            // 涙ぐましい(´；ω；｀)
+            KanColleWidget.Stash.questlistCalled = true;
+            break;
         default:
             //_log('%c[ACTION]%c Do Nothing for this request',true);
     }
@@ -118,6 +122,17 @@ CompleteDispatcher.prototype.execute = function(){
     } else if (this.requestSequence[0] == 'api_get_master/mission') {
         this.action.forMissionPreparation();
     } else if (this.requestSequence[0] == 'api_get_member/record') {
-        this.action.forKousyouPreparation();
+        var action = this.action;
+        // 工廠画面への遷移が「record」単体なのに対して
+        // 任務画面への遷移は「record→questlist」である
+        // したがって「直後にquestlistが呼ばれたか」を
+        // 判定する必要がある(´；ω；｀)ﾌﾞﾜｯ
+        setTimeout(function(){
+            if (KanColleWidget.Stash.questlistCalled) {
+                KanColleWidget.Stash.questlistCalled = false;
+                return "do nothing";
+            }
+            action.forKousyouPreparation();
+        },500);
     }
 };
