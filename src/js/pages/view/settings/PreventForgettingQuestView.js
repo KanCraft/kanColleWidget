59d39cb0f657a553fe79545d1b80479b34459123
otiai10 <otiai10@gmail.com>
2013-12-02 00:49:43 +0900

    Add description for PreventForgettingQuestView

--- src/js/pages/view/settings/PreventForgettingQuestView.js
+++ src/js/pages/view/settings/PreventForgettingQuestView.js
@@ -8,6 +8,7 @@ var widgetPages = widgetPages || {};
     var PreventForgettingQuestView = widgetPages.PreventForgettingQuestView = function(){
         this.inputName = 'prevent-forgetting-quest';
         this.title = "デイリー任務受け忘れ防止アラートを出す";
+        this.description = '<span class="description xsmall"><b>1</b>.戦績表示するときに工廠が反応しちゃう <b>2</b>.改装は検知できない です</sapn>';
     };
     PreventForgettingQuestView.prototype = Object.create(widgetPages.SettingCheckboxView.prototype);
     PreventForgettingQuestView.prototype.constructor = PreventForgettingQuestView;
