b204e17e3a204e3e685e1490f4956747f72224b1
otiai10 <otiai10@gmail.com>
2013-12-06 13:52:11 +0900

    Refactor & Test : OcrServer

--- src/js/definitions/models/server/ServerBase.js
+++ src/js/definitions/models/server/ServerBase.js
@@ -1,63 +1,65 @@
 /* jshint indent: 4 */
+var KanColleWidget = KanColleWidget || {};
+(function(){
+    "use strict";
+    /**
+     * サーバ通信する基底クラス
+     * @param constants {Object} src/js/Constants.js
+     */
+    var ServerBase = KanColleWidget.ServerBase = function(constants) {
+        this.constants = constants;
+    };
+    /**
+     * サーバにPOSTを送る
+     * @param dict
+     * @param callback
+     * @protected
+     */
+    ServerBase.prototype._post = function(dict, callback){
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', this.url);
 
-/**
- * サーバ通信する基底クラス
- * @param constants {Object} src/js/Constants.js
- */
-var ServerBase = function (constants) {
-    this.constants = constants;
-};
+        var self = this;
+        xhr.addEventListener('load', function() {
 
-/**
- * サーバにPOSTを送る
- * @param dict
- * @param callback
- * @protected
- */
-ServerBase.prototype._post = function(dict, callback){
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', this.url);
+            if(xhr.status !== 200) {
+                // これココ？
+                window.alert(
+                    'server : ' + self.url + '\n' +
+                        'status : ' + xhr.status + '\n' +
+                        'text : ' + xhr.statusText + ',サーバエラーっぽい');
+                return;
+            }
 
-    var self = this;
-    xhr.addEventListener('load', function() {
+            var response = JSON.parse(xhr.response);
+            response.status = xhr.status;
+            return callback(response);
+        });
 
-        if(xhr.status !== 200) {
-            // これココ？
-            alert(
-                'server : ' + self.url + '\n' +
-                'status : ' + xhr.status + '\n' +
-                'text : ' + xhr.statusText + ',サーバエラーっぽい');
-            return;
+        // TODO : Flaskサーバはx-www-form-urlencodeをdecodeする必要がある
+        // TODO : とりあえず今はこちらからencodeせずに送る
+        // FIXME: うんこーど
+        if (this.url.match('log-kcwidget')) {
+            xhr.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded');
         }
+        xhr.send(this.dict2ParamStr(dict));
+    };
 
-        var response = JSON.parse(xhr.response);
-        response.status = xhr.status;
-        return callback(response);
-    });
-
-    // TODO : Flaskサーバはx-www-form-urlencodeをdecodeする必要がある
-    // TODO : とりあえず今はこちらからencodeせずに送る
-    // FIXME: うんこーど
-    if (this.url.match('log-kcwidget')) {
-        xhr.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded');
-    }
-    xhr.send(this.dict2ParamStr(dict));
-};
-
-/**
- * 辞書型をポストパラメータ文字列に変換する
- * @param dict
- * @returns {string}
- */
-ServerBase.prototype.dict2ParamStr = function(dict){
-    var params = [];
-    for(var name in dict){
-        if(dict.hasOwnProperty(name)) {
-            var value = dict[name];
-            var param = encodeURIComponent( name ).replace( /%20/g, '+' )
-                + '=' + encodeURIComponent( value ).replace( /%20/g, '+' );
-            params.push( param );
+    /**
+     * 辞書型をポストパラメータ文字列に変換する
+     * @param dict
+     * @returns {string}
+     */
+    ServerBase.prototype.dict2ParamStr = function(dict){
+        var params = [];
+        for(var name in dict){
+            if(dict.hasOwnProperty(name)) {
+                var value = dict[name];
+                var param = encodeURIComponent( name ).replace( /%20/g, '+' )
+                    + '=' + encodeURIComponent( value ).replace( /%20/g, '+' );
+                params.push( param );
+            }
         }
-    }
-    return params.join( '&' );
-};
+        return params.join( '&' );
+    };
+})();
