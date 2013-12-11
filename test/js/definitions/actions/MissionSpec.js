d64ce0ea145c9035d5c8cc066a0f906a5811ac9d
otiai10 <otiai10@gmail.com>
2013-12-11 22:34:24 +0900

    Add test for Action

---
+++ test/js/definitions/actions/MissionSpec.js
@@ -0,0 +1,25 @@
+describe("MissionAction", function() {
+  var missionAction;
+  var achievements;
+  beforeEach(function() {
+    MyStorage.ofTest();
+    missionAction = new KanColleWidget.MissionAction();
+    Config.set("record-achievements", true);
+    achievements = new KanColleWidget.Achievements(MyStorage.ofTest());
+    achievements.update();
+  });
+
+  describe("`forStart`", function() {
+    it("should increment achievements.", function() {
+      expect(achievements.toJson()['daily']['contents']['mission_count']).toEqual(0);
+      console.log(missionAction);
+      console.log(Constants);
+      missionAction.forStart({api_mission_id:[1],api_deck_id:2});
+      expect(achievements.toJson()['daily']['contents']['mission_count']).toEqual(1);
+    });
+  });
+
+  afterEach(function() {
+    MyStorage.ofTest().tearDown();
+  });
+});
