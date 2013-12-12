a53331f0db13bdba27b68e1e93df9ba15f681cba
otiai10 <otiai10@gmail.com>
2013-12-13 03:32:03 +0900

    DELETE LEGACY!!! :trollface:

--- src/js/pages/settings.js
+++ src/js/pages/settings.js
@@ -55,10 +55,6 @@ function affectConfigInView(){
         }
 
         var input = document.getElementById(key);
-        if(key.match(/^notification.*file$/)) {
-            input = document.getElementById(key + '-already-set');
-        }
-
         if(key.match(/volume/)) {
             input.value = config[key];
         }
@@ -72,8 +68,6 @@ function affectConfigInView(){
             if(config[key]) { input.innerHTML = '設定済み'; }
         }
     }
-    if(config['notification-img-file']) displayImgSrc(document.getElementById('notification-img'), config['notification-img-file']);
-    if(config['popup-bg-img-file']) displayImgSrc(document.getElementById('popup-bg-img'), config['popup-bg-img-file']);
 
     $('select').each(function() {
         var sel = $(this);
@@ -105,30 +99,6 @@ function bindConfigChangedAction(){
                 Config.set(this.id, this.value);
             });
             break;
-    	  case 'submit':
-            inputs[i].addEventListener('click', function() {
-                var kind = $(this).attr('data-kind');
-                testNotificationSound(kind);
-            });
-            break;
-    	  case 'file':
-        	  inputs[i].addEventListener('change',function(){
-                var target = document.getElementById(this.getAttribute('target'));
-                var purpose = this.id;
-                var accepts = this.accept.split(",");
-                Fs.update(purpose, this.files[0], accepts, function(res){
-                    if(res.status == 0){
-                        Config.set(res.purpose, '');
-                        validationMessage(res.message, purpose + '-validation');
-                    }else if(res.status == 1){
-                        Config.set(res.purpose, res.entry.toURL());
-                        Config.set(res.purpose + '-force', true);
-                        validationMessage( res.origin.name + 'に設定しました', purpose + '-validation');
-                    }
-                    affectConfigInView();
-                });
-        	  });
-        	  break;
     	  case 'range':
         	  inputs[i].addEventListener('change',function(){
                 var target = document.getElementById(this.getAttribute('target'));
@@ -159,50 +129,3 @@ function bindResetButtons(){
         });
     });
 }
-
-/* private void */function testNotificationSound(kind) {
-    var d = new Date();
-    var text = "通知テスト\n" + d.toLocaleDateString() + " " + d.toLocaleTimeString();
-    Util.presentation(text, {
-        sound: {
-            kind  : kind,
-            force : true
-        }
-    });
-}
-
-/* private void */function commitImgUrlToConfig(img_url){
-    Config.set('notification-img-url', img_url);
-}
-/* private void */function displayImgSrc(display, img_url){
-    display.src = img_url;
-}
-/* private bool */function validImageSrc(request_ok){
-    if(request_ok.getResponseHeader('Content-Type').match(/^image.*/)) return true;
-    return false;
-}
-/* private bool */function validURL(url){
-    if(!url) return false;
-    if(!url.match(/^http[s]?:\/\/.*\.jpg|jpeg|JPG|gif|GIF|png|PNG$/)) return false;
-    return true;
-}
-/* void */function validationMessage(text, id, opt){
-    if(typeof text == 'undefined') text = '';
-    if(typeof   id == 'undefined') id = 'notification-img-file-validation';
-    var mess = document.getElementById(id);
-    if(mess) mess.innerHTML = text;
-}
-/* string */function hideFileRootFromPath(path){
-    return path.replace(/^filesystem:chrome-extension:\/\/[a-zA-Z]*\/persistent\//, '');
-}
-
-$(function() {
-    $('#open-notification-sound-extend').click(function() {
-        $('#notification-sound-extend').slideDown();
-        $(this).css('display', 'none');
-    });
-    $('#close-notification-sound-extend').click(function() {
-        $('#notification-sound-extend').slideUp();
-        $('#open-notification-sound-extend').css('display','');
-    });
-});
