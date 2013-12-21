93d6fa3a8716715e45ec37b1beb192aaca301dec
otiai10 <otiai10@gmail.com>
2013-12-21 23:23:34 +0900

    Add alt-time hover effect

--- src/js/pages/view/dashboard/TimerView.js
+++ src/js/pages/view/dashboard/TimerView.js
@@ -19,35 +19,73 @@ var widgetPages = widgetPages || {};
         var name = this.namePrefix + this.event[this.primaryKey] + suffix;
         var params = {time:'--:--&nbsp;&nbsp;&nbsp;&nbsp;', name: name};
         if (this.event.finish) {
-            params.time = this.toText(this.event.finish);
+            params.time = this.toText();
         }
         this.apply(params)._render();
+
+        var self = this;
+        this.$el.find('time').hover(
+            function(ev){
+                if($('.alt-time').length > 0) $('.alt-time').remove();
+                self.alt = self['showAlternatives'](ev,self);
+            },
+            function(ev){
+                self.alt = self['removeAlternatives'](ev,self);
+            }
+        );
+
         return this.$el;
     };
-    TimerView.prototype.toText = function(finishtime){
+    TimerView.prototype.toText = function(isAlt){
 
-        var h,m;
+        var finishtime = this.event.finish;
 
-        if(true){
+        var h, m;
+        var optional = '';
+
+        var showRemainedTime = true;//Config.get('ほげほげ');
+
+        if(isAlt) showRemainedTime = ! showRemainedTime;
+
+        if(showRemainedTime){
             var diffMilliSec = finishtime - Date.now();
             var min = 60*1000;
             var hour = 60*min;
             m = parseInt((diffMilliSec % hour)/min);
             h = parseInt((diffMilliSec - m*min)/hour);
+            if (isAlt) {
+                optional = 'あと ';
+            }
         }else{
             var finishD = new Date(finishtime);
             h = Util.zP(2, finishD.getHours());
             m = Util.zP(2, finishD.getMinutes());
+
+            if (isAlt) {
+                optional = (1+finishD.getMonth())+'月'+finishD.getDate()+'日';
+            }
         }
 
-        return Util.zP(2,h) + ':' + Util.zP(2,m);
+        return optional + Util.zP(2,h) + ':' + Util.zP(2,m);
     };
     TimerView.prototype.openManualWindow = function(ev, self){
         var path = "src/html/set_manual_timer.html";
         var params = {};
         params[self.primaryKey] = self.event[self.primaryKey];
-        console.log(self);
         params['purpose'] = self.target;
         Util.enterTimeManually(params, path);
+    };
+    TimerView.prototype.showAlternatives = function(ev, self){
+
+        if (self.event.finish == null) return;
+
+        return $('<div></div>').addClass('alt-time').css({
+            left: (ev.clientX * 3) / 4,
+            top: ev.clientY - 50
+        }).append(self.toText(true)).hide().prependTo('body').fadeIn(80);
+    };
+    TimerView.prototype.removeAlternatives = function(ev, self){
+        if (! self.alt) return;
+        self.alt.fadeOut(80,function(){ $(this).remove() });
     };
 })();
