9d68ab6e85199577d42d3dec9e4a982c1525638d
otiai10 <otiai10@gmail.com>
2014-01-12 16:20:08 +0900

    Add notice for ShowShipsStatus

--- src/js/pages/view/settings/ShowShipsStatusView.js
+++ src/js/pages/view/settings/ShowShipsStatusView.js
@@ -6,7 +6,7 @@ var widgetPages = widgetPages || {};
     var ShowShipsStatusView = widgetPages.ShowShipsStatusView = function(){
         this.inputName = 'show-ships-status';
         this.title = "進撃/撤退選択時に艦娘の状態を表示する";
-        this.description = '<span class="description xsmall">大破進撃、ダメ。ゼッタイ。</sapn>';
+        this.description = '<span class="description xsmall">大破進撃、ダメ。ゼッタイ。<b>注意: ウィジェット窓でないと座標がずれます</b></sapn>';
     };
     ShowShipsStatusView.prototype = Object.create(widgetPages.SettingCheckboxView.prototype);
     ShowShipsStatusView.prototype.constructor = ShowShipsStatusView;
