294da93462af0a8c79871750919acf75236a635b
otiai10 <otiai10@gmail.com>
2014-01-13 15:02:37 +0900

    Fix bug : record achievements even if mission reminder disabled

--- src/js/pages/view/settings/DynamicReminderTypeView.js
+++ src/js/pages/view/settings/DynamicReminderTypeView.js
@@ -7,9 +7,9 @@ var widgetPages = widgetPages || {};
 
     var DynamicReminderTypeView = widgetPages.DynamicReminderTypeView = function(){
         this.inputName = 'dynamic-reminder-type';
-        this.title = "入渠・建造時リマインダー登録を有効にする";
+        this.title = "入渠・建造時完了通知を使う";
         this.elements = [
-            {value:'0', title: '何もしない', description: '入渠・建造のリマインダは使わないよというひと'},
+            {value:'0', title: '使わない', description: '入渠・建造のリマインダは使わないよというひと'},
             {value:'1', title: '常に手動登録ウィンドウ出す', description: '自動取得失敗とかウザいよってひと'},
             {value:'3', title: '自動取得するけど', description: '失敗しても手動登録ウィンドウ出さない'},
             {value:'2', title: '自動取得', description: '失敗したら手動登録ウィンドウが出ます'}
