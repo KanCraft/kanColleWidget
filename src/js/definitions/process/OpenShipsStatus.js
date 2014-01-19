ce81a4949681ce620e81da3f39870a445fddcdc3
otiai10 <otiai10@gmail.com>
2014-01-19 14:35:38 +0900

    Remove Black Blank in capturing

--- src/js/definitions/process/OpenShipsStatus.js
+++ src/js/definitions/process/OpenShipsStatus.js
@@ -30,12 +30,12 @@ var KanColleWidget = KanColleWidget || {};
         var img = new Image();
         img.src = dataURI;
 
-        var blank = this._getBlank(img.width, img.height);
+        var blank = Util.getBlank(img.width, img.height);
 
         var coordsAndSize = {
             coords : {
-                left : (img.width - blank.width)  * (1/5) + blank.left,
-                top  : (img.height - blank.height) * (3/8) + blank.top
+                left : (img.width - blank.width)  * (1/5) + blank.offsetLeft,
+                top  : (img.height - blank.height) * (3/8) + blank.offsetTop
             },
             size : {
                 width : (img.width - blank.width)  * (3/10),
@@ -43,21 +43,5 @@ var KanColleWidget = KanColleWidget || {};
             }
         };
         return coordsAndSize;
-    };
-
-    // とりあえずここでやるぞ！
-    // 余黒対応
-    OpenShipsStatus.prototype._getBlank = function(width, height) {
-        var screen = {width:width, height:height};
-        var aspect = 0.6;
-        var blank = {top:0, left:0, height:0, width:0};
-        if (screen.height / screen.width < aspect) {
-            blank.width = screen.width - (screen.height / aspect);
-            blank.left = blank.width / 2;
-        } else {
-            blank.height = screen.height - (screen.width * aspect);
-            blank.top = blank.height / 2;
-        }
-        return blank; 
     };
 })();
