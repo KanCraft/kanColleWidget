de972ec995d6c1b024f757620afeb81d5385f9c7
otiai10 <otiai10@gmail.com>
2014-01-30 05:30:08 +0900

    Add ajax loader

--- src/js/pages/view/select/StaffTwitterView.js
+++ src/js/pages/view/select/StaffTwitterView.js
@@ -2,7 +2,12 @@ var widgetPages = widgetPages || {};
 (function() {
     var StaffTwitterView = widgetPages.StaffTwitterView = function() {
         this.modTwitter = KanColleWidget.TwitterCrawler;
-        this.tpl = '<div><hr><h6>運営メンテナンス情報<span id="hit-count"></span></h6><div id="staff-tweets-contents"></div></div>';
+        this.tpl = ''
+        + '<div>'
+        + '  <hr><h6>運営メンテナンス情報<span id="hit-count"></span></h6>'
+        + '  <div id="staff-tweets-contents"></div>'
+        + '  <img id="ajax-loader" src="../img/ajax-loader.gif" style="display:none;width:100%">'
+        + '</div>';
         this.events = {
         };
         this.attrs = {
@@ -17,7 +22,10 @@ var widgetPages = widgetPages || {};
         return this.$el;
     };
     StaffTwitterView.prototype.addTweets = function(){
+        this.$el.find('#ajax-loader').show();
+        var self = this;
         this.modTwitter.get$(function($e){
+            self.$el.find('#ajax-loader').hide();
             $('div#staff-tweets-contents').hide().append($e).fadeIn(30);
             $('span#hit-count').text('(ヒット'+$e.length+'件)');
         });
