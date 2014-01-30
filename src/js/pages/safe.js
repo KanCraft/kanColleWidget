c3cac2fdedaed85ba811de356a9a3fa3f69ca329
otiai10 <otiai10@gmail.com>
2014-01-30 14:05:38 +0900

    adjust to Windows

--- src/js/pages/safe.js
+++ src/js/pages/safe.js
@@ -4,4 +4,10 @@ $(function(){
         top: -95,
         left: -70
     });
+    // adjust to windows
+    if (navigator.userAgent.match(/Win/) || navigator.platform.indexOf('Win') !== -1) {
+        var diffWidth = window.outerWidth - window.innerWidth;
+        var diffHeight = window.outerHeight - window.innerHeight;
+        window.resizeTo(window.outerWidth + diffWidth, window.outerHeight + diffHeight);
+    }
 });
