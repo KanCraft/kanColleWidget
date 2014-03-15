bd23f03a78fae162624682dce0fad0f8198b121e
otiai10 <otiai10@gmail.com>
2014-03-15 15:29:37 +0900

    [sync] 1.0.6.8

--- src/js/Constants.js
+++ src/js/Constants.js
@@ -1,19 +1,15 @@
 var Constants = {
     release : {
-        version: "v1.0.6.7",
+        version: "v1.0.6.8",
         link: 'https://github.com/otiai10/kanColleWidget/pull/380',
-        announceVersion : 48,
+        announceVersion : 49,
         announcements   : [
             '複数PCブラウザ間の同期設定を追加',
             '欲しかったので作りました',
-            'ウィジェット窓を閉じたときにセーブ、',
-            'ボタンからロードです。',
-            'それはそうと伊８ぜんぜん出ません',
-            '-----',
             '複数PC間同期を、タイマーだけとか出来るように修正しました',
-            '<a href="https://twitter.com/aramotokei/status/444422823059017728" class="light">新海域で伊８ドロップする</a>らしいのでほっとしました',
-            '-----',
-            '新しい遠征を追加'
+            '新しい遠征を追加',
+            '「うまく同期されない」という声が多かったので複数PC間同期の設定を刷新しました',
+            '再設定が必要な場合がありますが、ご了承ください'
         ]
     },
     area: ["鎮守府海域","南西諸島海域","北方海域","西方海域","南方海域"],
