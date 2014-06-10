5d5431543ebdec43f0b015bb8d6925fb0ccc37d3
t-kasamatsu <kasamatsu@shimapri.jp>
2014-06-10 19:58:44 +0900

    #356 WHITEモードのWindows用アジャスト処理を削除
    WHITEモード展開時に適正なサイズで出力されるため
    Windows用アジャスト処理を削除

--- src/js/pages/safe.js
+++ src/js/pages/safe.js
@@ -9,12 +9,7 @@ $(function(){
             'left'    : '-70px'
         },500);
     },500);
-    // adjust to windows
-    if (Util.system.isWindows()) {
-        var diffWidth = window.outerWidth - window.innerWidth;
-        var diffHeight = window.outerHeight - window.innerHeight;
-        window.resizeTo(window.outerWidth + diffWidth, window.outerHeight + diffHeight);
-    }
+
     history.forward();
 
     $(document).on('keyup',function(e){
