77b6f53fc9e3af7d87e7679edc07a6f536e1f3ef
otiai10 <otiai10@gmail.com>
2014-11-27 21:29:10 +0900

    Fix #485

--- src/js/pages/proxy.js
+++ src/js/pages/proxy.js
@@ -3,7 +3,7 @@
     'use strict';
     // ここもループにして複数回試行したほうがいいかな？
     setTimeout(function(){
-        var iframeUrl = document.getElementsByTagName('iframe').item(0).getAttribute('src');
+        var iframeUrl = document.getElementById("game_frame").getAttribute('src');
         // replace() メソッドを使うとブラウザに履歴を残さないので、ブラウザバックできなくできる
         location.replace(iframeUrl);
     }, 300);
