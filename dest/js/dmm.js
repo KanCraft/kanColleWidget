936ffed77f54c9e8ed3c123c450d1c3ddc953bfb
otiai10 <otiai10@gmail.com>
2016-05-19 22:19:54 +0200

    ensure `chrome` object and ES6 syntax

--- dest/js/dmm.js
+++ dest/js/dmm.js
@@ -1,1 +1,6 @@
-window.alert("おっぱい");
+let version = '001'
+window.alert(version);
+console.log('chrome', chrome);
+chrome.runtime.sendMessage(nil, {act: `version ${version}`}, function(res) {
+  console.log(res);
+});
