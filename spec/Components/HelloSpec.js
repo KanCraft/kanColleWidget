2f0dbe0e6a6a3bd9d1dd287c36cfbabdcf2d2afb
otiai10 <otiai10@gmail.com>
2016-05-20 18:02:47 +0200

    Update test to test components

--- spec/Components/HelloSpec.js
+++ spec/Components/HelloSpec.js
@@ -4,6 +4,9 @@ describe("Foo", () => {
     it("baz", () => {
       true.should.be.true;
       // true.should.be.false;
+      let hello = new Hello();
+      // hello.foo().should.equal.'Foo';
+      chai.expect(hello.foo()).to.equal('This is Hello.foo');
     })
   })
 })
---
+++ src/js/Components/Hello.js
@@ -0,0 +1,5 @@
+class Hello {
+  foo() {
+    return 'This is Hello.foo';
+  }
+}
