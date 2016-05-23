048c5c082377d9b07e3a2d66576908acae49ef12
otiai10 <otiai10@gmail.com>
2016-05-23 11:56:31 +0200

    Minimum apply for Material-UI

--- src/js/Components/Views/PopupView.js
+++ src/js/Components/Views/PopupView.js
@@ -1,7 +1,32 @@
 import React, { Component } from 'react';
 
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+
 export default class PopupView extends Component {
-    render() {
-      return <h1>ほげえええ</h1>;
-    }
+
+  constructor() {
+    super();
+    this.state = {
+      value: 4
+    };
+  }
+
+  handleChange(ev, index, value) {
+    alert(`${index}\n${value}`);
+  }
+
+  render() {
+    return (
+      <div>
+        <SelectField value={this.state.value} onChange={this.handleChange}>
+          <MenuItem value={1} primaryText="Never" />
+          <MenuItem value={2} primaryText="Every Night" />
+          <MenuItem value={3} primaryText="Weeknights" />
+          <MenuItem value={4} primaryText="Weekends" />
+          <MenuItem value={5} primaryText="Weekly" />
+        </SelectField>
+      </div>
+    );
+  }
 }
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,5 +1,19 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+import { createStore } from 'redux'
 import PopupView from './Components/Views/PopupView';
 
-render(<PopupView />, document.getElementById('main'));
+let store = createStore((state = {}, action) => {
+  return state;
+})
+
+render(
+  <MuiThemeProvider store={store}>
+    <PopupView />
+  </MuiThemeProvider>,
+  document.getElementById('main')
+);
