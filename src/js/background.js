45310024d835f1e0a383106e86317ab8a78ce9e6
otiai10 <otiai10@gmail.com>
2016-05-25 10:38:42 +0200

    Apply Chomex

--- src/js/background.js
+++ src/js/background.js
@@ -1,6 +1,31 @@
 import Hello from './Components/Hello';
-chrome.runtime.onMessage.addListener((a, b, c) => {
-  let hello = new Hello();
-  c({hoge: hello.foo()});
-  return true;
+import { MessageRouter, WebRequestRouter, Controller } from 'chomex';
+
+// chrome.runtime.onMessage.addListener((a, b, c) => {
+//   let hello = new Hello();
+//   c({hoge: hello.foo()});
+//   return true;
+// });
+
+let router = new MessageRouter();
+router.on('/foo', new Controller());
+router.on('/bar', new Controller());
+
+chrome.runtime.onMessage.addListener(router.listener());
+
+let reqrouter = new WebRequestRouter();
+// reqrouter.on((sequence) => {
+//   console.log('in match func!!', sequence);
+//   return true;
+// }, (detail) => {
+//   console.log('in controller!!', detail);
+// });
+reqrouter.on([{url: /api_port/}, {url: /api_get_member/}, {url: /api_port/}], (detail, sequence) => {
+  console.log('コントローラだよん', sequence);
 });
+
+chrome.webRequest.onBeforeRequest.addListener(
+  reqrouter.listener(),
+  {'urls':["*://*/kcsapi/*"]},
+  ['requestBody']
+);
--- src/js/dmm.js
+++ src/js/dmm.js
@@ -1,5 +1,11 @@
 console.log(chrome.runtime.getManifest());
 chrome.runtime.connect();
-chrome.runtime.sendMessage({msg: 'Hello, this is content script'}, (res) => {
+chrome.runtime.sendMessage({act: '/foo', name: 'Hello, this is content script'}, (res) => {
+  console.log("GET RESPONSE!!", res);
+});
+chrome.runtime.sendMessage({act: '/bar', name: 'baaaaar'}, (res) => {
+  console.log("GET RESPONSE!!", res);
+});
+chrome.runtime.sendMessage({act: '/baz', name: 'baaaaar'}, (res) => {
   console.log("GET RESPONSE!!", res);
 });
