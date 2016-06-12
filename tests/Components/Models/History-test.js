bb374db69dd9cc841128f691cb32b511eeac52d8
otiai10 <otiai10@gmail.com>
2016-06-12 20:08:02 +0200

    Mockは分ける

--- tests/Components/Models/History-test.js
+++ tests/Components/Models/History-test.js
@@ -2,22 +2,6 @@ jest.unmock('chomex');
 jest.unmock('../../../src/js/Components/Models/History');
 import History from '../../../src/js/Components/Models/History';
 
-// TODO: jest.mockにできる？
-Object.defineProperty(window, 'localStorage', { value: (function() {
-  var store = {};
-  return {
-    getItem: function(key) {
-      return store[key];
-    },
-    setItem: function(key, value) {
-      store[key] = value.toString();
-    },
-    clear: function() {
-      store = {};
-    }
-  };
-})()});
-
 describe('model History', () => {
   describe('class', () => {
     it('shold do something', () => {
