33b9095643c340bd848e31d8126db3b3d83b1bea
otiai10 <otiai10@gmail.com>
2016-07-24 13:50:52 +0200

    なんかリリーススクリプトがかっこいい

--- scripts/pre-release.js
+++ scripts/pre-release.js
@@ -3,13 +3,15 @@ var path     = require('path');
 var manifest = require('../manifest.json');
 var exec     = require('shelljs').exec;
 
-if (exec('git status --short')) {
+if (exec('git status --short').stdout) {
   throw '[艦これウィジェット][pre-release] git status が clean じゃないっぽい';
 }
 
+var previousVersion = manifest.version;
+
 // Extensionのversionをインクリメントする
 // -vで与えられてたらアレする
-manifest.version = ((version, args) => {
+var nextVersion = ((version, args) => {
   var i = args.findIndex(arg => {
     return arg === '-v';
   });
@@ -21,6 +23,8 @@ manifest.version = ((version, args) => {
   return versions.concat(minor + 1).join('.');
 })(manifest.version, process.argv);
 
+manifest.version = nextVersion;
+
 // プロジェクト直下のmanifestに反映
 fs.writeFile(
   path.join(path.dirname(__dirname), 'manifest.json'),
@@ -30,8 +34,20 @@ fs.writeFile(
   }
 );
 
-console.log(exec('git add manifest.json'));
-console.log(exec(`git commit -m '[${manifest.version}]'`));
+if (
+  exec(`git add manifest.json`).code !== 0
+  || exec(`git commit -m '[${manifest.version}]'`).code !== 0
+) {
+  // しっぱいしたので戻す
+  manifest.version = previousVersion;
+  fs.writeFile(
+    path.join(path.dirname(__dirname), 'manifest.json'),
+    JSON.stringify(manifest, null, 2),
+    (err) => {
+      if (err) throw err;
+    }
+  );
+}
 
 // リリース用のmanifestに反映
 fs.writeFile(
