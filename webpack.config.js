8606759dea170cc4fd8cca990b1681a518410715
otiai10 <otiai10@gmail.com>
2016-10-30 16:16:38 +0100

    webpack.config.js自体をちょっとリファクタした

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,40 @@
-var webpack = require('webpack');
+/* global process:false */
+var webpack = require("webpack");
 
 var plugins = [
-  new webpack.optimize.DedupePlugin(),
-  new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
-  })
+    new webpack.optimize.DedupePlugin(),
+    new webpack.DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "development")
+    })
 ];
-if (process.env.NODE_ENV == 'production') {
-  plugins.push(new webpack.optimize.UglifyJsPlugin({
-    compress: { warnings: false },
-    mangle: false
-  }));
+if (process.env.NODE_ENV == "production") {
+    plugins.push(new webpack.optimize.UglifyJsPlugin({
+        compress: { warnings: false },
+        mangle: false
+    }));
 }
 
 module.exports = {
-  entry: {
-    options:    './src/js/entrypoints/pages/options.js',
-    popup:      './src/js/entrypoints/pages/popup.js',
-    dmm:        './src/js/entrypoints/pages/dmm.js',
-    'osapi.dmm':'./src/js/entrypoints/pages/osapi.dmm.js',
-    capture:    './src/js/entrypoints/pages/capture.js',
-    deckcapture:'./src/js/entrypoints/pages/deckcapture.js',
-    status:     './src/js/entrypoints/pages/status.js',
-    background: './src/js/entrypoints/background.js',
-  },
-  output: {filename:'./dest/js/[name].js'},
-  module: {
-    loaders: [
-      {test: /.jsx?$/, loader: 'babel-loader'},
-      {test: /\.(otf|eot|svg|ttf|woff|woff2)(\?.+)?$/,loader: 'url'},
-      {test: /.json$/, loader: 'json'}
-    ]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  plugins: plugins
-}
+    entry: {
+        options:    "./src/js/entrypoints/pages/options.js",
+        popup:      "./src/js/entrypoints/pages/popup.js",
+        dmm:        "./src/js/entrypoints/pages/dmm.js",
+        osapi_dmm  :"./src/js/entrypoints/pages/osapi.dmm.js",
+        capture:    "./src/js/entrypoints/pages/capture.js",
+        deckcapture:"./src/js/entrypoints/pages/deckcapture.js",
+        status:     "./src/js/entrypoints/pages/status.js",
+        background: "./src/js/entrypoints/background.js",
+    },
+    output: {filename:"./dest/js/[name].js"},
+    module: {
+        loaders: [
+            {test: /.jsx?$/, loader: "babel-loader"},
+            {test: /\.(otf|eot|svg|ttf|woff|woff2)(\?.+)?$/,loader: "url"},
+            {test: /.json$/, loader: "json"}
+        ]
+    },
+    resolve: {
+        extensions: ["", ".js", ".jsx"]
+    },
+    plugins: plugins
+};
