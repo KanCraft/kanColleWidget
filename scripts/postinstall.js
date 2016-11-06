260b8eac033ed241697e7a2423e9f93b0199f055
otiai10 <otiai10@gmail.com>
2016-11-06 18:23:55 +0100

    シェルのワンライナーじゃなくてscriptsの中にpostinstallをJSで書いた
    Fix #619

---
+++ scripts/postinstall.js
@@ -0,0 +1,38 @@
+/* global process:false */
+const fs = require("fs");
+require("colors"); // String.{color} が使えるようになる
+
+const prefix = "[艦これウィジェット]";
+const appConfigFilePath = "./app-config.json";
+
+const createAppConfigFileIfNotExists = (fpath) => {
+    console.log(prefix.green, "app-configの作成".underline);
+    console.log(`${fpath} の存在チェックします`);
+    if (fs.existsSync(fpath)) return console.log("すでにファイルがあるのでそれを使います :)");
+    console.log(`${fpath} を新規作成します`);
+    const appConfigTemplate = {
+        "twitter": {
+            "request_url":     "https://api.twitter.com/oauth/request_token",
+            "authorize_url":   "https://api.twitter.com/oauth/authorize",
+            "access_url":      "https://api.twitter.com/oauth/access_token",
+            "consumer_key":    "https://apps.twitter.com/ でCreate New Appするともらえるやつ",
+            "consumer_secret": "https://apps.twitter.com/ でCreate New Appするともらえるやつ"
+        },
+        "google": {
+            "vision_api_url": "https://vision.googleapis.com/v1/images:annotate",
+            "api_key": "https://console.cloud.google.com/apis/api/vision.googleapis.com/overview でつくれるやつ"
+        }
+    };
+    fs.writeFileSync(fpath, JSON.stringify(appConfigTemplate, null, 2));
+    if (!fs.existsSync(fpath)) {
+        console.log("ファイルの作成に失敗しました :(".red);
+        process.exit(1);
+    }
+    console.log(`${appConfigFilePath} が作成されました :)`);
+    console.log("Twitter連携をデバッグする場合は https://apps.twitter.com/ で Consumer Key/Secret を作成してください".yellow);
+    console.log("GoogleVisionAPIのデバッグする場合は https://console.cloud.google.com/apis/api/vision.googleapis.com/overview で Google API Key を作成してください".yellow);
+};
+
+createAppConfigFileIfNotExists(appConfigFilePath);
+
+console.log(prefix.green, "開発の準備が整いました. next) npm start".underline);
