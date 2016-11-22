1758ceb5965717139b7ddf03b40e64ab0c3818e2
otiai10 <otiai10@gmail.com>
2016-11-22 17:35:20 +0100

    フリーハンド描画がガクガクしてたのでちょっとスムーズにした

--- src/js/Components/Views/Capture/Tools/Pencil.js
+++ src/js/Components/Views/Capture/Tools/Pencil.js
@@ -3,17 +3,23 @@ import Tool from "./Base";
 export default class Pencil extends Tool {
     constructor(canvas) {
         super(canvas);
+        this.width = 6;
     }
     onStart(ev) {
         super.onStart();
-        this.prev = this.position(ev);
+        this.context.lineWidth = this.width;
+        const start = this.position(ev);
+        this.drawPoint(start);
+        this.prev = start;
     }
     onMove(ev) {
         const next = this.position(ev);
         this.drawLine(next);
+        this.drawPoint(next);
         this.prev = next;
     }
-    onEnd() {
+    onEnd(ev) {
+        this.drawPoint(this.position(ev));
         super.onEnd();
     }
     drawLine(next) {
@@ -21,5 +27,10 @@ export default class Pencil extends Tool {
         this.context.moveTo(this.prev.x, this.prev.y);
         this.context.lineTo(     next.x,      next.y);
         this.context.stroke();
+    }
+    drawPoint(pos) {
+        this.context.beginPath();
+        this.context.arc(pos.x, pos.y, this.width/2, 0, 2 * Math.PI);
+        this.context.fill();
     }
 }
