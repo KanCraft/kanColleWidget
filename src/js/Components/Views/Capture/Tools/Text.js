bc70c21529df0b9cc3a88323d54f0a60306c7ee2
otiai10 <otiai10@gmail.com>
2016-11-23 12:24:51 +0100

    テキストツールの開始時にまずテキストを描画してほしい

--- src/js/Components/Views/Capture/Tools/Text.js
+++ src/js/Components/Views/Capture/Tools/Text.js
@@ -7,13 +7,15 @@ export default class Text extends Tool {
         this.text  = params.text  || "";
         this.font  = params.font  || "4em Arial";
     }
-    onStart() {
+    onStart(ev) {
         super.onStart();
         this.backup = this.context.getImageData(
           0, 0, this.canvas.width, this.canvas.height
         );
         this.context.fillStyle = this.color;
         this.context.font      = this.font;
+        const pos = this.position(ev);
+        this.context.fillText(this.text, pos.x, pos.y);
     }
     onMove(ev) {
         this.context.putImageData(this.backup, 0, 0);
