4c0efd49b8b4eef72be9c6c3f7fed71c81f4b54b
otiai10 <otiai10@gmail.com>
2016-12-09 21:59:58 +0100

    スイッチ形式の設定項目用コンポーネントをつくった

---
+++ src/js/Components/Views/Options/Settings/SettingSwitch.js
@@ -0,0 +1,40 @@
+import React, {Component, PropTypes} from "react";
+
+import {TableRow, TableRowColumn} from "material-ui/Table";
+import Toggle                     from "material-ui/Toggle";
+import Config                     from "../../../Models/Config";
+
+export default class SettingSwitch extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            model: Config.find(this.props.model)
+        };
+    }
+    render() {
+        return (
+          <TableRow>
+            <TableRowColumn>
+              {this.props.title}
+            </TableRowColumn>
+            <TableRowColumn>
+              <Toggle toggled={this.state.model.value} onToggle={this.onToggle.bind(this)}/>
+            </TableRowColumn>
+            <TableRowColumn>
+              {this.props.description}
+            </TableRowColumn>
+          </TableRow>
+        );
+    }
+    onToggle() {
+        let model = this.state.model;
+        model.value = !model.value;
+        model.save();
+        this.setState({model});
+    }
+    static propTypes = {
+        title:       PropTypes.string.isRequired,
+        description: PropTypes.string,
+        model:       PropTypes.string.isRequired
+    }
+}
