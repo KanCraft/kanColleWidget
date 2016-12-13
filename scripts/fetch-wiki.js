07ef5168e80a1e74c9ebc79d7ea67dffa61e396a
otiai10 <otiai10@gmail.com>
2016-12-13 08:32:48 +0100

    npm経由で`npm run fetch-wiki`で遠征情報をスクレイプするようにした

---
+++ scripts/fetch-wiki.js
@@ -0,0 +1,30 @@
+
+const Client = require("cheerio-httpcli");
+
+const fixed_missions = [
+    {id: "33",  title: "前衛支援任務(南方)",     time: 900000},
+    {id: "34",  title: "決戦支援任務(南方)",     time: 1800000},
+    {id: "197", title: "前衛支援任務(イベント)", time: 900000},
+    {id: "198", title: "決戦支援任務(イベント)", time: 1800000},
+    {id: "-1",  title: "DEBUG: 今すぐのやつ", time: 0},
+];
+
+
+Client.fetch("http://wikiwiki.jp/kancolle/?%B1%F3%C0%AC", (err, $, res) => {
+    const missions = $("h2#h2_content_1_4").next().next().find("tr").map((index, element) => {
+        const id    = $(element).find("td:nth-child(1)").text();
+        const title = $(element).find("td:nth-child(2)").text();
+        const time  = $(element).find("td:nth-child(3)").text();
+        if (id.match(/^\d+$/) && title.match(/^.+$/) && time.match(/^(\d+):(\d+)$/)) {
+            const ms = time.match(/^(\d+):(\d+)$/);
+            return {id: id, title: title, time: (Number(ms[1]) * 60 + Number(ms[2])) * 60 * 1000};
+        } else {
+            return undefined;
+        }
+    }).toArray().concat(fixed_missions);
+    var result = {};
+    for (index in missions) {
+        result[missions[index].id] = {title: missions[index].title, time: missions[index].time};
+    }
+    console.log(JSON.stringify(result, null, " "));
+});
