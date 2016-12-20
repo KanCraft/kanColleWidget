5396023e8ae8cc28fc60f987759ef367bc8717bc
otiai10 <otiai10@gmail.com>
2016-12-21 00:14:04 +0100

    window.Dateに「残り時間」を表示するメソッドをぶっこんだ

--- src/js/entrypoints/global-pollution.js
+++ src/js/entrypoints/global-pollution.js
@@ -3,8 +3,13 @@ export function init(context) {
     context.Number.prototype.zp = function(pad = "0", digits = 2) {
         return ((new Array(digits)).fill(pad).join("") + this).slice(-1 * digits);
     };
-    context.Date.prototype.toClockString = function() {
-        return this.toTimeString().split(":").slice(0,2).join(":");
+    context.Date.prototype.toClockString = function(rest = false) {
+        if (!rest) return this.toTimeString().split(":").slice(0,2).join(":");
+        const target = new Date();
+        let mins = Math.ceil((this.getTime() - target.getTime()) / (1000 * 60));
+        let hours = Math.floor(mins / 60);
+        mins = mins - (hours * 60);
+        return [(hours).zp("0", 2), (mins).zp("0", 2)].join(":");
     };
     context.Date.prototype.toRemainingMinutes = function(target = new Date()) {
         const rest = Math.ceil((this.getTime() - target.getTime()) / 1000 / 60);
