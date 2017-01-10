2020217c6e568e763ad6541bc7247f02365abe0e
otiai10 <otiai10@gmail.com>
2017-01-10 19:21:44 +0900

    画像URIの長さを考慮してスクショ画面のURLクエリを生成するRoutineを追加（テストつき！！）

--- src/js/Components/Controllers/CommandControllers/index.js
+++ src/js/Components/Controllers/CommandControllers/index.js
@@ -7,16 +7,28 @@ import LaunchPosition from "../../Models/LaunchPosition";
 import Config from "../../Models/Config";
 import Assets from "../../Services/Assets";
 
+function OpenCaptureWindow(uri) {
+    let params = new URLSearchParams();
+    if (uri.length > 1 * Math.pow(10, 6)) {
+        const hash = `kcw:tmp:deckimage:${Date.now()}`;
+        // TODO: chrome直接参照〜
+        chrome.storage.local.set({[hash]:uri}, () => {
+            params.set("datahash", hash);
+            window.open(chrome.extension.getURL("dest/html/capture.html") + "?" + params.toString());
+        });
+    } else {
+        params.set("img", uri);
+        window.open(chrome.extension.getURL("dest/html/capture.html") + "?" + params.toString());
+    }
+}
+
 export function CaptureController() {
     const assets = new Assets(Config);
     windows.find().then(tab => {
         return captures.capture(tab.windowId);
     }).then(uri => {
         if (Config.find("directly-download-on-capture").value) return assets.downloadImageURL(uri);
-        let params = new URLSearchParams();
-        params.set("img", uri);
-    // とりあえず
-        window.open(chrome.extension.getURL("dest/html/capture.html") + "?" + params.toString());
+        else OpenCaptureWindow(uri);
     });
 }
 
