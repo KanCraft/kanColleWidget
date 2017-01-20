33a63189fdd04ce1cc88bc45b911be8bcae9d363
otiai10 <otiai10@gmail.com>
2017-01-20 12:39:30 +0100

    ゲーム内アクションボタンは最初は非表示にする
    Related #688

--- src/js/Components/Routine/InAppActionButtons.js
+++ src/js/Components/Routine/InAppActionButtons.js
@@ -37,6 +37,7 @@ export default class InAppActionButtons {
         div.style.top      = 0;
         div.style.right    = 0;
         div.style.transition = "all 0.2s";
+        div.style.opacity  = 0;
         div.addEventListener("mouseover", () => { div.style.opacity = 1; });
         div.addEventListener("mouseleave", () => { div.style.opacity = 0; });
         div.appendChild(this.getMuteButton());
