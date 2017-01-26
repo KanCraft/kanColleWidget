9bd41602804cce99d109062d9891be13ca087e44
otiai10 <otiai10@gmail.com>
2017-01-26 07:51:01 +0100

    あたらしく定義した艦これ時間クラスを使うようにAchievementクラスを修正した

--- src/js/Components/Models/Achievement.js
+++ src/js/Components/Models/Achievement.js
@@ -1,39 +1,23 @@
 import {Model} from "chomex";
 
 import * as Const from "../../Constants";
+import KanColleDate from "../Services/KanColleDate";
 
 export default class Achievement extends Model {
-    static needsUpdateForDaily(lastTouched, _now = new Date()) {
-        let last = (new Date(lastTouched)).toJST();
-        let now  = _now.toJST();
-        // 日付が同じ場合、5時前を境界線として互いに異なる領域に存在している場合は更新が必要
-        if (last.getDate() == now.getDate()) return last.isBefore5AM() != now.isBefore5AM();
-        // 2日以上の日付の差がある場合は、問答無用で更新が必要
-        if (now.getDate() - last.getDate() > 1) return true;
-        // 以下、日付の差が1日
-        // 現在時刻がすでに5時過ぎていれば、前日以前の最終更新は無効なので更新が必要
-        if (!now.isBefore5AM()) return true;
-        // あるいは最終更新が5時前であれば、いずれにしても古いので更新が必要
-        if (last.isBefore5AM()) return true;
-        // 以下、日付の差が1日であり、かつ現在時刻が5時より前、かつ最終更新が5時より後
-        return false;
-    }
     static daily() {
+        let now = new KanColleDate();
         let daily = this.find("daily");
-        if (this.needsUpdateForDaily(daily.lastTouched)) {
+        if (now.needsUpdateForDaily(daily.lastTouched)) {
             daily.records = this.default.daily.records;
         }
         daily.lastTouched = Date.now();
         daily.save();
         return daily;
     }
-    // TODO: 超だるいロジックをここに書く...
-    static needsUpdateForWeekly() {
-        return false;
-    }
     static weekly() {
+        let now = new KanColleDate();
         let weekly = this.find("weekly");
-        if (this.needsUpdateForWeekly(weekly.lastTouched)) {
+        if (now.needsUpdateForWeekly(weekly.lastTouched)) {
             weekly.records = this.default.weekly.records;
         }
         weekly.lastTouched = Date.now();
