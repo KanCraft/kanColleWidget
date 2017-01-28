d8eccf535a78bf7fd84daa99736b43a5ad7c5d8c
otiai10 <otiai10@gmail.com>
2017-01-28 11:40:21 +0100

    ゲーム内ボタン表示のリファクタ

--- src/js/Components/Routine/InAppActionButtons.js
+++ src/js/Components/Routine/InAppActionButtons.js
@@ -6,41 +6,45 @@ export default class InAppActionButtons {
         this.client  = client;
         this.context = context;
         this.muted   = this.tab.mutedInfo.muted;
-    }
-    static create(tab, client, context) {
-        return new this(tab, client, context);
-    }
-    getButtonContainer() {
-        let div = this.context.document.createElement("div");
-        return div;
-    }
-    getMuteButton() {
-        let container = this.getButtonContainer();
-        let img = this.context.document.createElement("img");
-        img.style.cursor = "pointer";
-        img.style.width = "36px";
-        img.style.height = "36px";
-        img.style.opacity = 0.8;
-        img.src = `chrome-extension://${this.extId}/dest/img/icons/vol_${this.muted ? "off" : "on"}.png`;
-        img.addEventListener("click", () => {
-            this.client.message("/window/mute", {tab: this.tab, mute: !this.muted}).then(res => {
-                this.muted = res.tab.mutedInfo.muted;
-                img.src = `chrome-extension://${this.extId}/dest/img/icons/vol_${this.muted ? "off" : "on"}.png`;
-            });
-        });
-        container.appendChild(img);
-        return container;
-    }
-    html() {
+
         let div = this.context.document.createElement("div");
         div.style.position = "fixed";
         div.style.top      = 0;
         div.style.right    = 0;
-        div.style.transition = "all 0.2s";
+        div.style.transition = "all 0.1s";
         div.style.opacity  = 0;
         div.addEventListener("mouseover", () => { div.style.opacity = 1; });
         div.addEventListener("mouseleave", () => { div.style.opacity = 0; });
-        div.appendChild(this.getMuteButton());
+        this.container = div;
+    }
+    static create(tab, client, context) {
+        return new this(tab, client, context);
+    }
+    wrap(img) {
+        let div = this.context.document.createElement("div");
+        div.appendChild(img);
         return div;
+    }
+    getBaseImageTemplate() {
+        let img = this.context.document.createElement("img");
+        img.style.cursor = "pointer";
+        img.style.height = `${(this.context.innerHeight / 10)}px`;
+        img.style.opacity = 0.92;
+        return img;
+    }
+    getMuteButton() {
+        let img = this.getBaseImageTemplate();
+        img.src = `chrome-extension://${this.extId}/dest/img/icons/vol_${this.muted ? "off" : "on"}.svg`;
+        img.addEventListener("click", () => {
+            this.client.message("/window/mute", {tab: this.tab, mute: !this.muted}).then(res => {
+                this.muted = res.tab.mutedInfo.muted;
+                img.src = `chrome-extension://${this.extId}/dest/img/icons/vol_${this.muted ? "off" : "on"}.svg`;
+            });
+        });
+        return this.wrap(img);
+    }
+    html() {
+        this.container.appendChild(this.getMuteButton());
+        return this.container;
     }
 }
