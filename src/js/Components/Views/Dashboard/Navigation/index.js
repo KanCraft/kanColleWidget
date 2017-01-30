8392bdd0ad2823e319a8c295ab48aa544993794a
otiai10 <otiai10@gmail.com>
2017-01-30 08:15:18 +0100

    遠征帰投回収してないおしらせアニメーション
    TODO: 設定でon/offできるようにする

--- src/js/Components/Views/Dashboard/Navigation/index.js
+++ src/js/Components/Views/Dashboard/Navigation/index.js
@@ -10,11 +10,32 @@ import Assignment       from "material-ui/svg-icons/action/assignment";
 
 import {VTabs, VTabItem} from "./VerticalTabs";
 
+import {ScheduledQueues} from "../../../Models/Queue/Queue";
+
 export class MenuNavigation extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            schedule: {}
+        };
+    }
+    componentDidMount() {
+        this.interval = setInterval(() => {
+            const actives = ScheduledQueues.dict()["mission"].filter(q => !!q.scheduled);
+            if (actives.length == 3) {
+                this.setState({schedule: {}});
+            } else {
+                this.setState({schedule: {animation:"SHAKE 0.5s infinite"}});
+            }
+        }, 5 * 1000);
+    }
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
     render() {
         return (
           <VTabs selectedIndex={this.props.index} onItemSelected={this.props.select}>
-            <VTabItem icon={<Schedule         />} />
+            <VTabItem icon={<Schedule style={this.state.schedule} />} />
             <VTabItem icon={<PlaylistAddCheck />} />
             <VTabItem icon={<Timeline         />} />
             <VTabItem icon={<Assignment       />} />
