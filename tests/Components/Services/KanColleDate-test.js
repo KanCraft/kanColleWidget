7524bc61584f5fff85904bc098cdaa9eac0a17f8
otiai10 <otiai10@gmail.com>
2017-02-08 01:55:33 +0100

    テストをパスしている限りは動いているということにしようじゃないか
    バグがあったら、まずバグを証明するテストを書きましょう、ということでここはひとつ手打ち。
    Fix #739

--- tests/Components/Services/KanColleDate-test.js
+++ tests/Components/Services/KanColleDate-test.js
@@ -22,6 +22,18 @@ describe("KanColleDate", () => {
             lastTouched = (new Date("Tue Feb 07 2017 19:00:00 GMT+0100 (CET)")).getTime();
             expect(d.needsUpdateForDaily(lastTouched)).toBe(true);
         });
+        it("ちょうど1ヶ月開いてしまって日にちが同じであってもちゃんと動く", () => {
+            const x = new Date("Wed Feb 08 2017 12:00:00 GMT+0100 (CET)");
+            const d = new KanColleDate(x);
+            let lastTouched = (new Date("Sun Jan 08 2017 00:00:00 GMT+01:00 (CET)")).getTime();
+            expect(d.needsUpdateForDaily(lastTouched)).toBe(true);
+        });
+        it("月をまたいでしまって日にちの大小関係が逆転してもちゃんと動く", () => {
+            const y = new Date("Wed Feb 08 2017 01:00:00 GMT+0100 (CET)");
+            const d = new KanColleDate(y);
+            let lastTouched = (new Date("Thu Jan 12 2017 00:00:00 GMT+01:00 (CET)")).getTime();
+            expect(d.needsUpdateForDaily(lastTouched)).toBe(true);
+        });
     });
     describe("needsUpdateForWeekly", () => {
         it("月曜5時を基準にアップデートが必要かどうかを返す", () => {
