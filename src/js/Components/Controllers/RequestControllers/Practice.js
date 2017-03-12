3af616ef41d2b82dbdd17ebea573f5de71c7609e
otiai10 <otiai10@gmail.com>
2017-03-12 14:22:10 +0100

    任務着手忘れアラートの処理をリファクタした

--- src/js/Components/Controllers/RequestControllers/Practice.js
+++ src/js/Components/Controllers/RequestControllers/Practice.js
@@ -1,17 +1,12 @@
 import Achievement from "../../Models/Achievement";
-import Config from "../../Models/Config";
-import Quest, {YET} from "../../Models/Quest";
 import {PRACTICE}  from "../../../Constants";
-import NotificationService from "../../Services/NotificationService";
+
+import {checkQuestStatus} from "./common";
 
 export function onPracticeStart() {
   Achievement.increment(PRACTICE);
 }
 
 export function onPracticePrepare() {
-  const quests = Quest.daily(false).filter(q => q.trigger == PRACTICE && q.state == YET);
-  if (quests.length && Config.find("quest-manager-alert").value) {
-    const notifications = new NotificationService();
-    notifications.create(...Quest.alert(quests));
-  }
+  checkQuestStatus(PRACTICE);
 }
