a883006679e796ae8b88053a7712acdf0be51fdd
otiai10 <otiai10@gmail.com>
2017-03-12 14:42:54 +0100

    近代化改装の任務着手忘れアラート（ただし事後）
    FEATURE: 任務着手忘れ防止アラートの残りを（たぶん）全部実装した
    
    Fix #787

--- src/js/Components/Controllers/RequestControllers/Kaisou.js
+++ src/js/Components/Controllers/RequestControllers/Kaisou.js
@@ -1,6 +1,8 @@
 import Achievement from "../../Models/Achievement";
 import {CUSTOMIZE} from "../../../Constants";
+import {checkQuestStatus} from "./common";
 
 export function onKaisouPowerup(/* detail */) {
   Achievement.increment(CUSTOMIZE);
+  checkQuestStatus(CUSTOMIZE);
 }
