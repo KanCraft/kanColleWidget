bebde365614149d06875153f183a6931e0897404
otiai10 <otiai10@gmail.com>
2017-03-13 09:49:17 +0100

    鎮守府実績記録をSAVE/LOADできるようにした
    Related #790

--- src/js/Components/Views/Options/Settings/Syncs/index.js
+++ src/js/Components/Views/Options/Settings/Syncs/index.js
@@ -57,6 +57,13 @@ export default class SyncSettingsView extends Component {
               </TableRowColumn>
             </TableRow>
             <TableRow>
+              <TableRowColumn>鎮守府実績記録</TableRowColumn>
+              <TableRowColumn>
+                <Toggle toggled={this.state.config.keys.has("Achievement")}
+                      onToggle={() => this.onToggle("Achievement")}/>
+              </TableRowColumn>
+            </TableRow>
+            <TableRow>
               <TableRowColumn>
                 <RaisedButton label="save" icon={<CloudUpload />} style={{marginRight: "12px"}} onClick={this.save.bind(this)}/>
                 <RaisedButton label="load" labelPosition="before" icon={<CloudDownload />}      onClick={this.load.bind(this)}/>
