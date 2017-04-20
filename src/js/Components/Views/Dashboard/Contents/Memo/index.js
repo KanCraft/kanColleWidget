7e04d3008514e7f59664bc6c0a97d2c417a95040
Hiromu OCHIAI <otiai10@gmail.com>
2017-04-20 18:03:09 +0900

    FEATURE: カスタムカウンター機能の実装
    Fix 796

--- src/js/Components/Views/Dashboard/Contents/Memo/index.js
+++ src/js/Components/Views/Dashboard/Contents/Memo/index.js
@@ -1,6 +1,10 @@
 import React, {Component,PropTypes} from "react";
 import RaisedButton from "material-ui/RaisedButton";
+import Dialog from "material-ui/Dialog";
+import TextField from "material-ui/TextField";
 import Memo from "../../../../Models/Memo";
+import Counter from "../../../../Models/Counter";
+import CounterView from "./CounterView";
 
 class MemoView extends Component {
   constructor(props) {
@@ -21,7 +25,7 @@ class MemoView extends Component {
     const height = (this.lines + 1) * (this.fontSize + 4) + "px";
     return (
       <textarea
-            style={{width:"100%", fontSize: this.fontSize + "px", height}}
+            style={{width:"98%", fontSize: this.fontSize + "px", height}}
             onChange={this.onChangeMemoText.bind(this)}
             value={this.state.text.value}
             >
@@ -31,13 +35,47 @@ class MemoView extends Component {
 }
 
 export default class DashboardMemo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      label: "",
+      error: "",
+      list:  Counter.list(),
+    };
+  }
+  showNewCounterDialog() {
+    this.setState({open:true});
+  }
+  onCreateCommit() {
+    if (this.state.label.length == 0) {
+      return this.setState({error:"ラベルを入力してください"});
+    }
+    Counter.create({label:this.state.label, count:0});
+    this.setState({list:Counter.list(), open:false});
+  }
   render() {
+    const styles = {
+      plus: {
+        display:"flex", justifyContent:"center",alignItems:"center",cursor:"pointer",fontSize:"1.2em",marginTop:"12px",
+      }
+    };
     return (
       <div style={{...this.props.style}}>
-        <div style={{marginRight:"28px"}}>
-          <MemoView />
+        <div style={{marginRight:"28px", display:"flex"}}>
+          <div style={{flex:2}}>
+            <MemoView />
+            <RaisedButton label="WIKIの早見表" onClick={() => window.open("/dest/html/wiki.html")} />
+          </div>
+          <div style={{flex:1}}>
+            {this.state.list.map((c, i) => <CounterView key={i} counter={c} refresh={() => this.setState({list:Counter.list()})} />)}
+            <div style={styles.plus} onClick={this.showNewCounterDialog.bind(this)}>+</div>
+          </div>
         </div>
-        <RaisedButton label="WIKIの早見表" onClick={() => window.open("/dest/html/wiki.html")} />
+        <Dialog open={this.state.open} onRequestClose={() => this.setState({open:false})}>
+          <TextField fullWidth={true} hintText={"ラベル"} errorText={this.state.error} onChange={(ev, val) => this.setState({label:val})}/>
+          <RaisedButton style={{float:"right"}} label="カスタムカウンター作成" onClick={this.onCreateCommit.bind(this)}/>
+        </Dialog>
       </div>
     );
   }
