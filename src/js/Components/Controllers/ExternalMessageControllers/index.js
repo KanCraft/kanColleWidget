7461c4959ecb0270da5fc3e9601bfb9bf6f1309f
50円 <opu50yen@gmail.com>
2017-04-26 07:26:30 +0900

    CI用Push

--- src/js/Components/Controllers/ExternalMessageControllers/index.js
+++ src/js/Components/Controllers/ExternalMessageControllers/index.js
@@ -8,7 +8,7 @@ export function SubscribeStart() {
   const allow_list = Subscriber.list();
   for (var i = 0; i < allow_list.length; i++) {
     if (allow_list[i]._id == sub._id) {
-      return {status:304,message:"Your application is already subscribed."};
+      return {status:304, message:"Your application is already subscribed."};
     }
   }
   if (!window.confirm(`Extension ID "${sub._id}" が『艦これウィジェット』との連携を要求しています。許可しますか？`)) {
