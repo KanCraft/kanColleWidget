25a967b0465f069d48bc7bbe3f168d7c5900ff0d
Hiromu OCHIAI <otiai10@gmail.com>
2017-05-03 14:11:16 +0900

    Fix bug on OCR loading window

--- src/js/pages/loader.js
+++ src/js/pages/loader.js
@@ -1,27 +1,27 @@
-var count = 0;
-var maxCount = 10;
-var getInnerText = function(){
-    var text = '取得中';
-    if(count == maxCount){
-        count = 0;
-        return text;
-    }
-    for(var i=0;i<count;i++){
-        text += '.';
-    }
-    count++;
-    return text;
-};
-var incrementDot = function(){
-    $('title').html(getInnerText());
-};
+// var count = 0;
+// var maxCount = 10;
+// var getInnerText = function(){
+//     var text = '取得中';
+//     if(count == maxCount){
+//         count = 0;
+//         return text;
+//     }
+//     for(var i=0;i<count;i++){
+//         text += '.';
+//     }
+//     count++;
+//     return text;
+// };
+// var incrementDot = function(){
+//     $('title').html(getInnerText());
+// };
 
 (function(){
     var image = Util.getLoaderBackgroundImage();
     var url = chrome.extension.getURL('/') + image.url;
     $('html').css({backgroundImage:'url("'+ url +'")'});
     $('html').hide().fadeIn(500);
-    setInterval(incrementDot,100);
+    // setInterval(incrementDot,100);
     $('div#answer').html(image.title);
     $('div#wrapper').on('mouseenter',function(){
         $('div#answer').show();
