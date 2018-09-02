3dab2a85fe5a6b37213f621234df33c570dba828
Hiromu Ochiai <otiai10@gmail.com>
2018-09-02 14:30:57 +0900

    右上ポップアップに、最小限の窓ローンチUIを実装（ガワだけ）

--- src/js/Applications/Components/Popup.vue
+++ src/js/Applications/Components/Popup.vue
@@ -1,13 +1,17 @@
 <template>
-  <div>
-    <h1 @click="foobar">ふえええ</h1>
+  <div class="container popup" style="width: 320px;">
+    <launch-trigger />
   </div>
 </template>
 <script lang="ts">
-import { Vue, Component, Prop } from "vue-property-decorator";
+import { Vue, Component } from "vue-property-decorator";
+import LaunchTrigger from "./Popup/LaunchTrigger.vue";
 
-// FIXME: Unexpected character '@'
-@Component
+@Component({
+  components: {
+    "launch-trigger": LaunchTrigger,
+  }
+})
 export default class Popup extends Vue {
   foobar() {
     alert("hogeeeee");
---
+++ src/js/Applications/Components/Popup/LaunchTrigger.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="columns">
+    <div class="column col-10">
+      <div class="form-group">
+        <select class="form-select" style="-webkit-appearance: none;">
+          <option>大型</option>
+          <option>中型</option>
+          <option>小型</option>
+        </select>
+      </div>
+    </div>
+    <div class="column col-2">
+      <div class="icon-justify clickable">
+        <!-- <img class="popup-icon" src="/dest/img/icons/compass.svg" /> -->
+        <compass class="fill-primary" width="28px" />
+      </div>
+      <!-- <button class="btn btn-action"><i class="icon icon-arrow-right"></i></button> -->
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import {Vue, Component} from "vue-property-decorator";
+import Compass from "../Icons/compass.vue";
+
+@Component({
+  components: {
+    compass: Compass,
+  }
+})
+export default class LaunchTrigger extends Vue {
+
+}
+</script>
