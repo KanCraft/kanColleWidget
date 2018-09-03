1d3d6c41c441938e3a245889196f3a217a239279
Hiromu Ochiai <otiai10@gmail.com>
2018-09-03 11:46:07 +0900

    content_scriptsの構造をリファクタリング

---
+++ src/js/Applications/Context/DMM.ts
@@ -0,0 +1,59 @@
+import {Client} from "chomex";
+import Const from "../../Constants";
+import Frame from "../Background/Models/Frame";
+
+/**
+ * http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/
+ * において実行されるアプリケーションの実装です。
+ */
+export default class DMM {
+
+  private tab: chrome.tabs.Tab;
+  private frame: Frame;
+
+  constructor(private scope: Window) {
+  }
+
+  /**
+   * 画面のロード時に1度だけ呼ばれることを想定された初期化ルーチン。
+   */
+  public async init() {
+    const client = new Client(chrome.runtime, false);
+    const {status, data} = await client.message("/window/decoration");
+    if (status < 200 && 300 <= status) {
+      return;
+    }
+    if (!data) {
+      return;
+    }
+
+    const {tab, frame} = data;
+    this.tab = tab;
+    this.frame = frame;
+
+    this.resizeToAdjustAero();
+    this.shiftFrame();
+  }
+
+  /**
+   * エアロ領域の計算と微調整
+   */
+  private resizeToAdjustAero() {
+    this.scope.resizeBy(
+      this.scope.outerWidth - Math.floor(this.scope.innerWidth * this.frame.zoom),
+      this.scope.outerHeight - Math.floor(this.scope.innerHeight * this.frame.zoom),
+    );
+  }
+
+  /**
+   * ゲーム表示領域を画面ぴったりに移動する
+   */
+  private shiftFrame() {
+    const iframe = this.scope.document.querySelector(Const.GameIFrame) as HTMLIFrameElement;
+    const rect = iframe.getBoundingClientRect() as DOMRect;
+    iframe.style.position = "relative";
+    iframe.style.left = "0";
+    iframe.style.top = `-${rect.y + Const.TopSpacing}px`;
+  }
+
+}
--- src/js/entrypoints/dmm.ts
+++ src/js/entrypoints/dmm.ts
@@ -1,30 +1,10 @@
-import {Client} from "chomex";
-import Const from "../Constants";
+import DMM from "../Applications/Context/DMM";
 
-async function main() {
-  const client = new Client(chrome.runtime, false);
-  const {status, data} = await client.message("/window/decoration");
-  if (status < 200 && 300 <= status) {
-    return; // do nothing
-  }
-  if (!data) {
-    return; // do nothing
-  }
-
-  // エアロ領域の計算と微調整
-  const {tab, frame} = data;
-  window.resizeBy(
-    window.outerWidth - Math.floor(window.innerWidth * frame.zoom),
-    window.outerHeight - Math.floor(window.innerHeight * frame.zoom),
-  );
-
-  // ゲーム領域を画面ぴったりにずらす
-  const iframe = document.querySelector(Const.GameIFrame) as HTMLIFrameElement;
-  const rect = iframe.getBoundingClientRect() as DOMRect;
-  iframe.style.position = "relative";
-  iframe.style.left = "0";
-  iframe.style.top = `-${rect.y + Const.TopSpacing}px`;
-
+declare interface App {
+  init(): any;
 }
 
-window.onload = main;
+((scope: Window) => {
+  const app: App = new DMM(scope);
+  scope.onload = () => app.init();
+})(window);
