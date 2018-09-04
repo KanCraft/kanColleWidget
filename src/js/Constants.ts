a291ca44a0127f090c8a1c8b9cf6f381cc4a5e39
Hiromu Ochiai <otiai10@gmail.com>
2018-09-04 19:10:32 +0900

    ゲーム領域の大きさを定数として書き出し

--- src/js/Constants.ts
+++ src/js/Constants.ts
@@ -1,14 +1,22 @@
 
-const GameIFrame: string = "iframe#game_frame";
 const KanColleURL: string = "http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/";
+
+const GameIFrame: string = "iframe#game_frame";
+const GameWidth: number = 1200;
+const GameHeight: number = 720;
 
 // なんかiframeのなかに
 // <div id="spacing_top" style="height:16px;"></div>
 // とかいうのがあるんだけど、いまどきこんなんやる？？？
 const TopSpacing: number = 16;
 
+/* tslint:disable object-literal-sort-keys */
 export default {
-  GameIFrame,
   KanColleURL,
+
+  GameIFrame,
+  GameWidth,
+  GameHeight,
+
   TopSpacing,
 };
