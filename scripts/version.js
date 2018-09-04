6869ab35745c3471d33106c69ef1a04144fdc703
Hiromu Ochiai <otiai10@gmail.com>
2018-09-04 19:45:16 +0900

    バージョンの自動更新スクリプトをいい感じにした

--- scripts/version.js
+++ scripts/version.js
@@ -60,21 +60,73 @@ function defineNextVersion(pkg, mnf, new_version_name) {
   return [mnf.version.replace(/\.[^.]+$/, `.${minor}`), mnf.version_name.replace(/\.[^.]+$/, `.${minor}`)];
 }
 
+function getReleaseInfo() {
+  const latest = shell.execSync("git describe --tags --abbrev=0").toString().trim();
+  const commits = shell.execSync(`git log --pretty='%H %s' ${latest}..HEAD`).toString().trim().split("\n").slice(1);
+  const authors = shell.execSync(`git log --pretty=%an ${latest}..HEAD`).toString().trim().split("\n").filter((v, i, a) => a.indexOf(v) == i);
+  return {commits, authors};
+}
+
 /**
  * main
  */
-function main(new_version_name) {
+function main(flags) {
   const pkg = getPackagejson();
   const mnf = getManifestjson();
-  const [version, version_name] = defineNextVersion(pkg, mnf, new_version_name);
+  const [version, version_name] = defineNextVersion(pkg, mnf, flags.get("version"));
   pkg.version = version_name;
   mnf.version = version;
   mnf.version_name = version_name;
   writePackagejson(pkg);
   writeManifestjson(mnf);
   shell.execSync("npm install");
+
+  if (flags.get("commit")) {
+    const files = ["package.json", "package-lock.json", "manifest.json"];
+    shell.execSync(`git add ${files.join(" ")} && git commit -m '${version_name}'`);
+  }
+
+  if (flags.get("tag")) {
+    const release = getReleaseInfo();
+    const commits = "Commits\n" + release.commits.map(c => "- " + c).join("\n");
+    const authors = "Authors\n" + release.authors.map(a => "- " + a).join("\n");
+    shell.execSync(`git tag ${version_name} -m '${commits}' -m '${authors}'`);
+  }
+
+  console.log("VERSION:", version_name);
+}
+
+class Flags {
+  constructor(args) {
+    this.dictionary = {};
+    args.map((arg, i) => {
+      const key = arg.replace(/^-+/, "");
+      const next = args[i+1];
+      if (next && !/^-+/.test(next)) {
+        this.dictionary[key] = next;
+      } else {
+        this.dictionary[key] = true;
+      }
+    });
+  }
+  get(key) {
+    return this.dictionary[key];
+  }
+  usage() {
+    return `次のバージョンを自動的に決めて、manifest.json, packae.json, package-lock.json を編集するやつ。
+
+オプション:
+  -commit: あたらしいバージョンを、git commit までする
+  -tag:    あたらしいバージョンで、git tag までする
+
+使用例:
+  npm run version -- -commit -tag
+`;
+  }
 }
 
 if (require.main == module) {
-  main(process.argv[2]);
+  const flags = new Flags(process.argv.slice(2));
+  if (flags.get("help")) return console.log(flags.usage());
+  main(flags);
 }
