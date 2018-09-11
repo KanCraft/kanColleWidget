a03896b8f1a4cc7f394a8bd1a9ce094d1d0a9ad5
Hiromu OCHIAI <otiai10@gmail.com>
2018-09-11 14:37:05 +0900

    しょうもないリネーム

--- src/js/Applications/Background/Controllers/Window.ts
+++ src/js/Applications/Background/Controllers/Window.ts
@@ -17,17 +17,17 @@ import Frame from "../../Models/Frame";
  * なければ、指定されたフレームか、最後に指定されたフレームにリサイズする.
  */
 export async function WindowOpen(message: any) {
-  const wins = WindowService.getInstance();
+  const ws = WindowService.getInstance();
   const id = message.id;
   const frame = Frame.find<Frame>(id) || Frame.latest();
-  let tab = await wins.find();
+  let tab = await ws.find();
   if (!!tab) {
-    tab = await wins.reconfigure(tab, frame);
+    tab = await ws.reconfigure(tab, frame);
     return Client.for(chrome.tabs, tab.id).message("/reconfigured", {frame});
   }
   frame.update({selectedAt: Date.now()});
-  tab = await wins.create(frame);
-  tab = await wins.zoom(tab, 1.0);
+  tab = await ws.create(frame);
+  tab = await ws.zoom(tab, 1.0);
   return tab;
 }
 
@@ -36,8 +36,8 @@ export async function WindowOpen(message: any) {
  * 必要な設定などをまとめて返す。
  */
 export async function WindowDecoration(message: any) {
-  const wins = WindowService.getInstance();
-  const launched = wins.knows(this.sender.tab.id);
+  const ws = WindowService.getInstance();
+  const launched = ws.knows(this.sender.tab.id);
   if (!launched) {
     return Promise.reject({status: 404});
   }
@@ -50,8 +50,8 @@ export async function WindowDecoration(message: any) {
 
 export async function WindowRecord(message: any) {
   const frame = Frame.find(message.frame.id);
-  const wins = WindowService.getInstance();
-  const win = await wins.get(this.sender.tab.windowId);
+  const ws = WindowService.getInstance();
+  const win = await ws.get(this.sender.tab.windowId);
   return frame.update({position: {left: win.left, top: win.top}});
 }
 
