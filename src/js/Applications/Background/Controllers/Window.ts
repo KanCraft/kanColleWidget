d9c66f7468ad1374e87d40e6052467b4457cc370
Hiromu OCHIAI <otiai10@gmail.com>
2018-09-11 17:45:30 +0900

    よかれと思っていれたzoom: 1.0がエラー出すのでやめた

--- src/js/Applications/Background/Controllers/Window.ts
+++ src/js/Applications/Background/Controllers/Window.ts
@@ -27,7 +27,7 @@ export async function WindowOpen(message: any) {
   }
   frame.update({selectedAt: Date.now()});
   tab = await ws.create(frame);
-  tab = await ws.zoom(tab, 1.0);
+  // tab = await ws.zoom(tab, 1.0);
   return tab;
 }
 
