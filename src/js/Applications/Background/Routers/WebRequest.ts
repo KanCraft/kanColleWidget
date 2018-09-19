a38b11406d087557badbbbfeddaa7b84b4a3fec9
Hiromu OCHIAI <otiai10@gmail.com>
2018-09-20 03:21:51 +0900

    E-2の航空戦開始時でも前回の大破進撃防止窓を閉じるようにした

--- src/js/Applications/Background/Routers/WebRequest.ts
+++ src/js/Applications/Background/Routers/WebRequest.ts
@@ -20,7 +20,8 @@ const router = new SerialRouter(2, resolver);
 // 母港寄港
 router.on(["api_port/port"], OnPort);
 // 戦闘終了
-router.on(["api_req_sortie/battle"],       OnBattleStarted);
-router.on(["api_req_sortie/airbattle"], OnAirBattleStarted);
+router.on(["api_req_sortie/battle"],          OnBattleStarted);
+router.on(["api_req_sortie/airbattle"],    OnAirBattleStarted);
+router.on(["api_req_sortie/ld_airbattle"], OnAirBattleStarted); // イベント中の航空戦
 
 export default router.listener();
