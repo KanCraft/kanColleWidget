28063eb2becfb70f0a773de494fa1512771377f6
Hiromu Ochiai <otiai10@gmail.com>
2018-09-23 17:53:53 +0900

    設定画面に大破進撃防止窓の設定を追加

--- src/js/Applications/Components/Options/InApp.vue
+++ src/js/Applications/Components/Options/InApp.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="category">
     <h1>ゲーム画面の設定</h1>
+    <config v-bind:config="damagesnapshot" />
     <config v-bind:config="muteButton" />
     <config v-bind:config="screenshotButton" />
     <config v-bind:config="buttonsPosition" />
@@ -19,12 +20,18 @@ import ConfigView from "./Config/index.vue";
 })
 export default class InApp extends Vue {
 
+  // TODO: chomex.Modelのバグが治ったら、
+  //       Config.filter(c => c.category == Category.InApp)
+  //       みたいな感じでもっとちゃんとする
+  //       https://github.com/otiai10/chomex/issues/39
+  private damagesnapshot: Config<string>;
   private buttonsPosition: Config<string>;
   private muteButton: Config<boolean>;
   private screenshotButton: Config<boolean>;
 
   constructor() {
     super();
+    this.damagesnapshot = Config.find("damagesnapshot");
     this.buttonsPosition = Config.find("inapp-buttons-position");
     this.muteButton = Config.find("inapp-mute-button");
     this.screenshotButton = Config.find("inapp-screenshot-button");
