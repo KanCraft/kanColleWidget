43390641ffd8681f5b2d5956cecd6fedd9f78443
Hiromu Ochiai <otiai10@gmail.com>
2018-09-24 18:21:28 +0900

    とりあえずE5の空襲マスで前回の艦隊状況が消えないバグが修正されるっぽい気がする変更を加えた

--- src/js/Applications/Background/Routers/WebRequest.ts
+++ src/js/Applications/Background/Routers/WebRequest.ts
@@ -19,12 +19,13 @@ const router = new SerialRouter(2, resolver);
 
 // 母港寄港
 router.on(["api_port/port"], OnPort);
-// 戦闘終了
-router.on(["api_req_sortie/battle"],          OnBattleStarted);
-router.on(["api_req_combined_battle/battle"], OnBattleStarted); // イベント中の連合艦隊
+// 戦闘開始時
+router.on(["api_req_sortie/battle"],                OnBattleStarted);
+router.on(["api_req_combined_battle/battle"],       OnBattleStarted); // イベント中の連合艦隊
 router.on(["api_req_combined_battle/battle_water"], OnBattleStarted); // イベント中の連合艦隊
-router.on(["api_req_combined_battle/each_battle"], OnBattleStarted); // イベント中の連合艦隊
-router.on(["api_req_sortie/airbattle"],    OnAirBattleStarted);
-router.on(["api_req_sortie/ld_airbattle"], OnAirBattleStarted); // イベント中の航空戦
+router.on(["api_req_combined_battle/each_battle"],  OnBattleStarted); // イベント中の連合艦隊
+router.on(["api_req_sortie/airbattle"],          OnAirBattleStarted);
+router.on(["api_req_sortie/ld_airbattle"],       OnAirBattleStarted); // イベント中の航空戦
+router.on(["api_req_map/start_air_base"],        OnAirBattleStarted); // イベントの基地航空戦？
 
 export default router.listener();
