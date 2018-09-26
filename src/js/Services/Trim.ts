ec1bbe273edf694549b222233e1a947ec550e09a
Hiromu Ochiai <otiai10@gmail.com>
2018-09-27 00:21:48 +0900

    よりテストしやすく修正

--- src/js/Services/Trim.ts
+++ src/js/Services/Trim.ts
@@ -8,7 +8,7 @@ export default class TrimService {
   public static init(imgURL): Promise<TrimService> {
     const s: TrimService = new TrimService();
     s.img = s.scope.createElement("img");
-    s.img.src = imgURL;
+    setTimeout(() => s.img.src = imgURL);
     return new Promise(resolve => {
       s.img.onload = () => resolve(s);
     });
@@ -23,7 +23,8 @@ export default class TrimService {
     this.canvas = this.scope.createElement("canvas");
     this.canvas.width = rect.size.w;
     this.canvas.height = rect.size.h;
-    this.canvas.getContext("2d").drawImage(
+    const ctx: CanvasRenderingContext2D = this.canvas.getContext("2d");
+    ctx.drawImage(
       this.img,
       rect.start.x, rect.start.y, rect.size.w, rect.size.h,
       0, 0, this.canvas.width, this.canvas.height,
