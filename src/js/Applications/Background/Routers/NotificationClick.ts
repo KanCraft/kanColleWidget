b58a6fc2fa5b38da5725362b42dc4c2807aedae1
ISIGE, Akira <xia@silvia.com>
2018-10-02 19:52:34 +0900

    入渠通知と建造通知もひとまず「クリックで消去」を実装

--- src/js/Applications/Background/Routers/NotificationClick.ts
+++ src/js/Applications/Background/Routers/NotificationClick.ts
@@ -13,4 +13,14 @@ router.on("Mission", async (id) => {
   await ns.clear(id);
 });
 
+router.on("Recovery", async (id) => {
+  const ns = new NotificationService();
+  await ns.clear(id);
+});
+
+router.on("Shipbuilding", async (id) => {
+  const ns = new NotificationService();
+  await ns.clear(id);
+});
+
 export default router.listener();
