dbb2bb5ff27fe08487c9d924c64e24a30939dcfd
Hiromu OCHIAI <otiai10@gmail.com>
2019-02-13 17:49:19 +0900

    なんか原因わからないエラーが有るのでLocalStorageインスペクターをつくった

--- src/js/entrypoints/options.tsx
+++ src/js/entrypoints/options.tsx
@@ -1,6 +1,41 @@
 import React from "react";
 import {render} from "react-dom";
 
+// webpack.config.jsの、DefinePluginを参照
+declare var NODE_ENV
+(() => {
+  const toggler: HTMLButtonElement = document.querySelector("#btn-localstorage-inspector");
+  if (NODE_ENV == "production") {
+    toggler.remove();
+    return;
+  }
+  let hidden: boolean = true;
+  const container: HTMLDivElement = document.querySelector("#localstorage-inspector-container");
+  const inspector: HTMLDivElement = container.querySelector("#localstorage-inspector");
+  const show = () => {
+    container.style.display = 'block';
+    const dictionary: any = {};
+    for (let key in window.localStorage) {
+      if (!window.localStorage.hasOwnProperty(key)) continue;
+      const raw = window.localStorage.getItem(key)
+      try {
+        const value = JSON.parse(raw);
+        dictionary[key] = value;
+      } catch (err) {
+        dictionary[key] = raw;
+      }
+    }
+    inspector.innerHTML = JSON.stringify(dictionary, null, 4);
+  };
+  const hide = () => {
+    container.style.display = 'none';
+  };
+  toggler.addEventListener("click", () => {
+    hidden = !hidden;
+    hidden ? hide() : show();
+  });
+})();
+
 import App from "../Applications/Components/Options";
 
 render(<App />, document.querySelector("main#app"));
