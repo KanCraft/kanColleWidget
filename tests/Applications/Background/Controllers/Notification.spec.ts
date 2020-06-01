d9d2dc63008c68d1c1c92292bb3019a5e3393db7
Hiromu Ochiai <otiai10@gmail.com>
2020-06-02 08:56:26 +0900

    新規にchrome API呼んでたところのスタブを追加

--- tests/Applications/Background/Controllers/Notification.spec.ts
+++ tests/Applications/Background/Controllers/Notification.spec.ts
@@ -4,9 +4,11 @@ import {fake} from "../../../tools";
 describe("OnNotificationClick", () => {
   it("なんかする", async (done) => {
     fake(chrome.notifications.clear).callbacks({});
-    fake(chrome.runtime.sendMessage).callbacks({});
-    fake(chrome.tabs.query).callbacks([]);
+    fake(chrome.tabs.sendMessage).callbacks({});
+    fake(chrome.tabs.query).callbacks([{ id: 12345 }]);
     fake(chrome.windows.create).callbacks({tabs:[]});
+    fake(chrome.windows.update).callbacks({});
+    fake(chrome.tabs.update).callbacks({});
     await OnNotificationClick("Test");
     done();
   });
