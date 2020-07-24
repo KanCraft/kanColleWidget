0aa0e683fee8fc3ee4a07f82420656a39398e0ba
Hiromu OCHIAI <otiai10@gmail.com>
2020-07-24 11:49:21 +0900

    TempStorageはchrome.storage.localを使い、storeはkeyをそのまま使う
    Related #1126

--- src/js/Services/TempStorage.ts
+++ src/js/Services/TempStorage.ts
@@ -1,33 +1,49 @@
 
+/**
+ * TempStorage
+ * 大きめの画像URIなんかを一時的に保存、取り出しと同時に削除することを目的としたもの.
+ * とくにスクショ編集画面に画像URIを伝えるケースで使う.
+ * 当初はlocalStorageを使っていたが、localStorageでは"unlimitedStorage"の恩恵を受けないので、
+ * chrome.storage.localを使っている.
+ */
 export default class TempStorage {
 
-  constructor(private storage: Storage = localStorage) {}
+  constructor(private storage = chrome.storage.local) {}
 
   static new(): TempStorage {
     return new this();
   }
 
-  private get(key: string): string {
-    return this.storage.getItem(key);
+  private get(key: string): Promise<string> {
+    return new Promise(resolve => {
+      this.storage.get(items => {
+        resolve(items[key]);
+      });
+    });
   }
 
   private delete(key: string) {
-    return this.storage.removeItem(key);
+    return new Promise(resolve => {
+      this.storage.remove(key, () => resolve());
+    });
   }
 
-  store(prefix: string, value: string, expires = 60 * 1000): string {
-    const key = prefix + "_" + Date.now();
-    this.storage.setItem(key, value);
+  store(key: string, value: string, expires = 60 * 1000): Promise<string> {
     setTimeout(() => this.delete(key), expires);
-    return key;
+    return new Promise(resolve => {
+      this.storage.get(items => {
+        items[key] = value;
+        this.storage.set(items, () => resolve(key));
+      });
+    });
   }
 
   /**
    * 取得し、消す。
    */
-  draw(key: string): string {
-    const value: string = this.get(key);
-    if (value) {
+  async draw(key: string, clean = true): Promise<string> {
+    const value: string = await this.get(key);
+    if (!!value && clean) {
       this.delete(key);
     }
     return value;
--- tests/Services/TempStorage.spec.ts
+++ tests/Services/TempStorage.spec.ts
@@ -1,13 +1,18 @@
 import TempStorage from "../../src/js/Services/TempStorage";
+import { fake } from "../tools";
 
 describe("TempStorage", () => {
   describe("ひととおり", () => {
-    it("なんかする", () => {
+    it("なんかする", async () => {
+      const key = "my_key_123";
+      const val = "my_val_456";
+      fake(chrome.storage.local.get).callbacks({ [key]: val });
+      fake(chrome.storage.local.set).callbacks({ [key]: val });
       const storage = new TempStorage();
-      const key = storage.store("my_prefix", "THIS IS VALUE");
-      expect(key).toMatch(/^my_prefix_.+$/);
-      const value = storage.draw(key);
-      expect(value).toBe("THIS IS VALUE");
+      const genkey = await storage.store(key, val);
+      expect(genkey).toBe(key);
+      const value = await storage.draw(key);
+      expect(value).toBe(val);
     });
   });
 });
