99cf6116926ed6bf0386d69398bb740dcd7ae962
Hiromu OCHIAI <otiai10@gmail.com>
2020-08-24 21:23:28 +0900

    ダッシュボードから任務進捗を手動変更できるようにした
    Fix #1175

--- src/js/Applications/Components/Dashboard/QuestProgressView/index.tsx
+++ src/js/Applications/Components/Dashboard/QuestProgressView/index.tsx
@@ -11,7 +11,13 @@ const sortmap: { [status: string]: number } = {
 
 export default class QuestProgressView extends React.Component<{
   progress: QuestProgress,
+}, {
+  manual: Quest,
 }> {
+  constructor(props) {
+    super(props);
+    this.state = { manual: null };
+  }
   render() {
     const { progress } = this.props;
     return (
@@ -24,22 +30,56 @@ export default class QuestProgressView extends React.Component<{
               </div>
               <div className="column quest-title">{quest.title}</div>
               <div className="column col-auto">
-                <span className={`status label ${quest.status}`}>{this.getStatusText(quest)}</span>
+                <span
+                  className={`c-hand status label ${quest.status}`}
+                  onClick={() => this.setState({ manual: quest })}
+                >{this.getStatusText(quest.status)}</span>
               </div>
             </div>
           );
         })}
+        {this.state.manual ? this.renderModal() : null}
       </div>
     );
   }
   questSortFunc(prev: Quest, next: Quest): number {
     return sortmap[prev.status] < sortmap[next.status] ? -1 : 1;
   }
-  getStatusText(quest: Quest): string {
-    switch (quest.status) {
+  getStatusText(status: Status): string {
+    switch (status) {
     case Status.Open: return "未着手";
     case Status.Ongoing: return "遂行中";
     case Status.Completed: return "達成";
     }
+  }
+  private renderModal() {
+    const quest = this.state.manual;
+    const progress = this.props.progress;
+    return (
+      <div className="modal modal-sm active quest-progress-manual">
+        <a className="modal-overlay" onClick={() => this.setState({ manual: null })} />
+        <div className="modal-container">
+          <div className="modal-header">
+            <div className="modal-title h5">{quest.title}</div>
+          </div>
+          <div className="modal-footer container">
+            <div className="columns">
+              <div className="column">
+                <button className={`btn ${Status.Open}`} onClick={() => progress.stop(quest.id) && this.closeModal()}>{this.getStatusText(Status.Open)}</button>
+              </div>
+              <div className="column">
+                <button className={`btn ${Status.Ongoing}`} onClick={() => progress.start(quest.id) && this.closeModal()}>{this.getStatusText(Status.Ongoing)}</button>
+              </div>
+              <div className="column">
+                <button className={`btn ${Status.Completed}`} onClick={() => progress.complete(quest.id) && this.closeModal()}>{this.getStatusText(Status.Completed)}</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  private closeModal() {
+    this.setState({ manual: null });
   }
 }
