0ae7093d79b36de381063a500e33488d9308e291
Hiromu OCHIAI <otiai10@gmail.com>
2020-09-24 23:53:41 +0900

    遠征ID非対応だった場合のインタラクションを追加

--- src/js/Applications/Models/Queue/Mission.ts
+++ src/js/Applications/Models/Queue/Mission.ts
@@ -48,4 +48,13 @@ export default class Mission extends Queue {
   getTimerLabel() {
     return `第${this.deck}艦隊 ${this.title}`;
   }
+
+  static notfoundNotification(mid: string): chrome.notifications.NotificationOptions {
+    const iconUrl = chrome.extension.getURL("dest/img/widget-chang.png");
+    return {
+      title: `遠征ID「${mid}」？ 知らない子ですね...`,
+      message: "遠征カタログへの追加要望をおねがいします",
+      iconUrl,
+    };
+  }
 }
