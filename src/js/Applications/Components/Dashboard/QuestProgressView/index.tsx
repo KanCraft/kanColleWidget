1425a9a944cf70ab8959452f73bb9f41b163fc8d
Hiromu OCHIAI <otiai10@gmail.com>
2020-09-27 09:50:12 +0900

    ダッシュボード任務一覧にデイリーとウィークリーのフィルター表示を実装

--- src/js/Applications/Components/Dashboard/QuestProgressView/index.tsx
+++ src/js/Applications/Components/Dashboard/QuestProgressView/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { QuestProgress, Quest } from "../../../Models/Quest";
-import { Status } from "../../../Models/Quest/consts";
+import { Group, Status } from "../../../Models/Quest/consts";
+import cn from "classnames";
 
 const sortmap: { [status: string]: number } = {
   [Status.Open]: 0,
@@ -13,16 +14,25 @@ export default class QuestProgressView extends React.Component<{
   progress: QuestProgress,
 }, {
   manual: Quest,
+  groups: { [group: string]: boolean },
 }> {
   constructor(props) {
     super(props);
-    this.state = { manual: null };
+    this.state = {
+      manual: null,
+      groups: { [Group.Daily]: true, [Group.Weekly]: true },
+    };
   }
   render() {
     const { progress } = this.props;
     return (
       <div className="container quest-progress">
-        {Object.values(progress.quests).filter(q => q.visible).sort(this.questSortFunc).map(quest => {
+        <div className="columns">
+          <div className="column col-auto btn-group btn-group-block btn-filters">
+            {[Group.Daily, Group.Weekly].map(group => this.renderGroupFilter(group))}
+          </div>
+        </div>
+        {this.getQuests(progress).map(quest => {
           return (
             <div className="columns" key={quest.id}>
               <div className="column col-auto">
@@ -42,12 +52,28 @@ export default class QuestProgressView extends React.Component<{
       </div>
     );
   }
+  renderGroupFilter(group: Group) {
+    const enabled = this.state.groups[group];
+    const className = cn("btn", "btn-sm", enabled ? "btn-primary" : "btn-link");
+    const onClick = () => this.setState({ groups: { ...this.state.groups, [group]: !this.state.groups[group] } });
+    return <button key={group} className={className} onClick={onClick}>{group}</button>;
+  }
+  getQuests(progress: QuestProgress): Quest[] {
+    return Object.values(progress.quests)
+      .filter(this.questFilterFunc.bind(this))
+      .sort(this.questSortFunc);
+  }
   onClickStatus(quest: Quest) {
     if (quest.status == Status.Completed) {
       this.props.progress.hide(quest.id);
     } else {
       this.setState({ manual: quest });
     }
+  }
+  questFilterFunc(quest: Quest): boolean {
+    if (!quest.visible) return false;
+    if (!this.state.groups[quest.group]) return false;
+    return true;
   }
   questSortFunc(prev: Quest, next: Quest): number {
     return sortmap[prev.status] < sortmap[next.status] ? -1 : 1;
