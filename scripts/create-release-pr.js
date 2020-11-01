ba216009eafd3aaa04a42cc624fcb99558bf573f
Hiromu OCHIAI <otiai10@gmail.com>
2020-11-01 15:55:54 +0900

    RELEASE PR 作成の自動化
    Fix #1172

--- scripts/create-release-pr.js
+++ scripts/create-release-pr.js
@@ -2,10 +2,8 @@
 const github = require("@actions/github");
 
 async function main() {
-  // const { owner, repo } = github.context.repo
   const { owner, repo } = { owner: "KanCraft", repo: "kanColleWidget" };
   const token = process.env.GITHUB_TOKEN;
-
   const octokit = github.getOctokit(token);
 
   // 直近のリリース上のコミットハッシュを取得
@@ -19,18 +17,21 @@ async function main() {
     const message = commit.message.split("\n")[0];
     if (message.startsWith("Merge pull request")) return null;
     if (message.match(/^[0-9]+\.[0-9]+\.[0-9]/)) return null;
-    return { hash: exp.exec(commit.url)[1], message };
+    return { hash: exp.exec(commit.url)[1], author: commit.author.name, message };
   }).filter(c => !!c);
 
-  // TODO: すでに "RLEASE PR" が開いているなら、bodyの更新、無いならPRの作成をする
-  const res = await octokit.pulls.create({
-    owner, repo,
-    title: `RELEASE: ${process.env.NEW_TAG}`,
-    head: "develop",
-    base: "main",
-    body: commits.map(c => `${c.hash} ${c.message}`).join("\n"),
-  });
-  console.log(res.data);
+  // すでに "RLEASE PR" が開いているなら、bodyの更新、無いならPRの作成をする
+  const head = "develop", base = "main";
+  const title = `RELEASE: ${process.env.NEW_TAG || (new Date()).toLocaleDateString()}`;
+  const body = commits.map(c => `${c.hash} (${c.author}) ${c.message}`).join("\n");
+  const exists = await octokit.pulls.list({ owner, repo, head, base, state: "open" })
+  if (exists.data.length) {
+    const res = await octokit.pulls.update({ owner, repo, title, body, pull_number: exists.data[0].number });
+    console.log("[INFO] RELEASE PR UPDATED:", res.data.html_url);
+  } else {
+    const res = await octokit.pulls.create({ owner, repo, head, body, base, title });
+    console.log("[INFO] RELEASE PR CREATED:", res.data.html_url);
+  }
 };
 
 if (require.main === module) {
