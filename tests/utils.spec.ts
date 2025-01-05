fe57e09bccf36a4df7f91edaa0ea8457e9b75500
Hiromu OCHIAI <otiai10@gmail.com>
2025-01-05 18:08:42 +0900

    テストの修正

--- tests/utils.spec.ts
+++ tests/utils.spec.ts
@@ -5,7 +5,7 @@ import { KCWDate } from "../src/utils";
 describe("format", () => {
   it("should format date correctly", () => {
     const date = new KCWDate("August 25, 2024 07:00:00");
-    expect(date.format("yyyy/mm/dd HH:MM:SS")).toBe("2024/08/25 07:00:00");
+    expect(date.format("YYYY/mm/dd HH:MM:SS")).toBe("2024/08/25 07:00:00");
   });
 });
 
@@ -13,6 +13,6 @@ describe("ETA", () => {
   it("should calculate ETA correctly", () => {
     const date = new KCWDate("August 25, 2024 07:00:00");
     const eta = KCWDate.ETA(1000 * 60 * 60, date.getTime());
-    expect(eta.format("yyyy/mm/dd HH:MM:SS")).toBe("2024/08/25 08:00:00");
+    expect(eta.format("YYYY/mm/dd HH:MM:SS")).toBe("2024/08/25 08:00:00");
   });
 });
