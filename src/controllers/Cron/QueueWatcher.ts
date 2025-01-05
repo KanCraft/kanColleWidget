9bb8a172bf80d9b09a997e793d5966a0bd51edeb
Hiromu OCHIAI <otiai10@gmail.com>
2025-01-05 16:37:53 +0900

    ちょっとリファクタ

--- src/controllers/Cron/QueueWatcher.ts
+++ src/controllers/Cron/QueueWatcher.ts
@@ -4,16 +4,15 @@ import Queue from "../../models/Queue";
 export async function Once() {
   const log = new Logger("QueueWatcher");
   const queues = await Queue.list();
-  for (let i = 0; i < queues.length; i++) {
-    const queue = queues[i];
-    if (queue.scheduled < Date.now()) {
-      try {
-        const e = queue.entry();
-        await chrome.notifications.create(e.$n.id(), e.$n.options())
-        await queue.delete();
-      } catch (e) {
-        log.warn("Once:", e);
-      }
+  console.log(queues.length, queues);
+  for (const queue of queues) {
+    if (queue.scheduled > Date.now()) continue;
+    try {
+      const e = queue.entry();
+      await chrome.notifications.create(e.$n.id(), e.$n.options())
+      await queue.delete();
+    } catch (e) {
+      log.warn("Once:", e);
     }
   }
 }
