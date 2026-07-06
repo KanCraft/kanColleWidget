07ff323ecf272ef2ae2142a63f5fe091275d478e
ISIGE, Akira <xia@silvia.com>
2026-07-06 20:21:43 +0900

    任務の着手状況を追跡するQuestProgressモデルを追加する

--- tests/model-namespace.spec.ts
+++ tests/model-namespace.spec.ts
@@ -19,6 +19,7 @@ import { FileSaveConfig } from "../src/models/configs/FileSaveConfig";
 import { FleetCaptureConfig } from "../src/models/configs/FleetCaptureConfig";
 import { GameWindowConfig } from "../src/models/configs/GameWindowConfig";
 import { NotificationConfig } from "../src/models/configs/NotificationConfig";
+import { QuestProgress } from "../src/models/QuestProgress";
 
 // jstorm の Model は _namespace_ が未指定だとクラス名を chrome.storage.local のキーに使う。
 // minify でクラス名はビルドごとに変わる短縮名になるため、_namespace_ を明示しないモデルは
@@ -35,6 +36,7 @@ const models = [
   FleetCaptureConfig,
   GameWindowConfig,
   NotificationConfig,
+  QuestProgress,
 ] as const;
 
 describe("jstorm モデルの _namespace_", () => {
